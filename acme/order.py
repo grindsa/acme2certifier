5e5654f856c3550f8dbdb742878a9bbca010d23b
grindsa <grindelsack@gmail.com>
2018-12-25 15:13:03 +0100

    pylint optimizations

--- acme/order.py
+++ acme/order.py
@@ -16,11 +16,8 @@ class Order(object):
     def __init__(self, debug=None, srv_name=None, expiry=86400):
         self.server_name = srv_name
         self.debug = debug
-        self.account = Account(self.debug, srv_name)
         self.dbstore = DBstore(self.debug)
         self.nonce = Nonce(self.debug)
-        self.error = Error(self.debug)
-        self.signature = Signature(self.debug)
         self.expiry = expiry
         self.authz_path = 'acme/authz'
         self.order_path = 'acme/order'
@@ -87,8 +84,10 @@ class Order(object):
             # nonce check
             (code, message, detail) = self.nonce.check(protected_decoded)
             if not message:
-                aname = self.account.name_get(protected_decoded)
-                (sig_check, error, error_detail) = self.signature.check(content, aname)
+                account = Account(self.debug, self.server_name)
+                aname = account.name_get(protected_decoded)
+                signature = Signature(self.debug)
+                (sig_check, error, error_detail) = signature.check(content, aname)
                 if sig_check:
                     (error, order_name, auth_dic, expires) = self.add(payload_decoded, aname)
                     if not error:
@@ -120,7 +119,8 @@ class Order(object):
         if not code == 201:
             if detail:
                 # some error occured get details
-                detail = self.error.enrich_error(message, detail)
+                error_message = Error(self.debug)
+                detail = error_message.enrich_error(message, detail)
                 response_dic['data'] = {'status':code, 'message':message, 'detail': detail}
             else:
                 response_dic['data'] = {'status':code, 'message':message, 'detail': None}
