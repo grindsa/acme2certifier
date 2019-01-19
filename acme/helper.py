1562bbd31565b85108ae463e14a53821717d8040
grindsa <grindelsack@gmail.com>
2019-01-19 18:34:53 +0100

    Update helper.py

--- acme/helper.py
+++ acme/helper.py
@@ -19,20 +19,6 @@ import pytz
 from jwcrypto import jwk, jws
 
 
-def get_url(environ, include_path=False):
-    """ get url """
-    server_name = environ['HTTP_HOST']
-    port = environ['SERVER_PORT']
-    if port == 443:
-        proto = 'https'
-    else:
-        proto = 'http'
-
-    if include_path:
-        return '{0}://{1}{2}'.format(proto, server_name, environ['PATH_INFO'])
-    else:
-        return '{0}://{1}'.format(proto, server_name)
-
 def b64decode_pad(debug, string):
     """ b64 decoding and padding of missing "=" """
     print_debug(debug, 'b64decode_pad()')
@@ -55,6 +41,43 @@ def b64_url_recode(debug, string):
     string += "="*padding_factor
     # return base64.b64decode(unicode(string).translate(dict(zip(map(ord, u'-_'), u'+/'))))
     return unicode(string).translate(dict(zip(map(ord, u'-_'), u'+/')))
+
+def build_pem_file(debug, existing, certificate, wrap):
+    """ construct pem_file """
+    print_debug(debug, 'build_pem_file()')
+    if existing:
+        if wrap:
+            pem_file = '{0}-----BEGIN CERTIFICATE-----\n{1}\n-----END CERTIFICATE-----\n'.format(existing, textwrap.fill(certificate, 64))
+        else:
+            pem_file = '{0}-----BEGIN CERTIFICATE-----\n{1}\n-----END CERTIFICATE-----\n'.format(existing, certificate)
+    else:
+        if wrap:
+            pem_file = '-----BEGIN CERTIFICATE-----\n{0}\n-----END CERTIFICATE-----\n'.format(textwrap.fill(certificate, 64))
+        else:
+            pem_file = '-----BEGIN CERTIFICATE-----\n{0}\n-----END CERTIFICATE-----\n'.format(certificate)
+    return pem_file
+
+def cert_san_get(debug, certificate):
+    """ get subject alternate names from certificate """
+    print_debug(debug, 'cert_san_get()')
+    pem_file = build_pem_file(debug, None, b64_url_recode(debug, certificate), True)
+    cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, pem_file)
+    san = []
+    ext_count = cert.get_extension_count()
+    for i in range(0, ext_count):
+        ext = cert.get_extension(i)
+        if 'subjectAltName' in str(ext.get_short_name()):
+            san.append(ext.__str__())
+
+    print_debug(debug, 'cert_san_get() ended')
+    return san
+
+def cert_serial_get(debug, certificate):
+    """ get serial number form certificate """
+    print_debug(debug, 'cert_serial_get()')
+    pem_file = build_pem_file(debug, None, b64_url_recode(debug, certificate), True)
+    cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, pem_file)
+    return cert.get_serial_number()
 
 def decode_deserialize(debug, string):
     """ decode and deserialize string """
@@ -98,12 +121,36 @@ def generate_random_string(debug, length):
     char_set = digits + ascii_letters
     return ''.join(random.choice(char_set) for _ in range(length))
 
+def get_url(environ, include_path=False):
+    """ get url """
+    server_name = environ['HTTP_HOST']
+    port = environ['SERVER_PORT']
+    if port == 443:
+        proto = 'https'
+    else:
+        proto = 'http'
+
+    if include_path:
+        return '{0}://{1}{2}'.format(proto, server_name, environ['PATH_INFO'])
+    else:
+        return '{0}://{1}'.format(proto, server_name)
+
 def load_config(debug=False, mfilter=None, cfg_file=os.path.dirname(__file__)+'/'+'acme_srv.cfg'):
     """ small configparser wrappter to load a config file """
     print_debug(debug, 'load_config({1}:{0})'.format(mfilter, cfg_file))
     config = configparser.ConfigParser()
     config.read(cfg_file)
     return config
+
+def parse_url(debug, url):
+    """ split url into pieces """
+    print_debug(debug, 'parse_url({0})'.format(url))
+    url_dic = {
+        'proto' : urlparse(url).scheme,
+        'host' : urlparse(url).netloc,
+        'path' : urlparse(url).path
+    }
+    return url_dic
 
 def print_debug(debug, text):
     """ little helper to print debug messages
@@ -147,16 +194,6 @@ def signature_check(debug, message, pub_key):
     # return result
     return(result, error)
 
-def parse_url(debug, url):
-    """ split url into pieces """
-    print_debug(debug, 'parse_url({0})'.format(url))
-    url_dic = {
-        'proto' : urlparse(url).scheme,
-        'host' : urlparse(url).netloc,
-        'path' : urlparse(url).path
-    }
-    return url_dic
-
 def uts_now():
     """ return unixtimestamp in utc """
     return calendar.timegm(datetime.utcnow().utctimetuple())
@@ -169,42 +206,10 @@ def date_to_uts_utc(date_human, tformat='%Y-%m-%dT%H:%M:%S'):
     """ convert date to unix timestamp """
     return int(calendar.timegm(time.strptime(date_human, tformat)))
 
-def build_pem_file(debug, existing, certificate, wrap):
-    """ construct pem_file """
-    print_debug(debug, 'build_pem_file()')
-    if existing:
-        if wrap:
-            pem_file = '{0}-----BEGIN CERTIFICATE-----\n{1}\n-----END CERTIFICATE-----\n'.format(existing, textwrap.fill(certificate, 64))
-        else:
-            pem_file = '{0}-----BEGIN CERTIFICATE-----\n{1}\n-----END CERTIFICATE-----\n'.format(existing, certificate)
-    else:
-        if wrap:
-            pem_file = '-----BEGIN CERTIFICATE-----\n{0}\n-----END CERTIFICATE-----\n'.format(textwrap.fill(certificate, 64))
-        else:
-            pem_file = '-----BEGIN CERTIFICATE-----\n{0}\n-----END CERTIFICATE-----\n'.format(certificate)
-    return pem_file
-
-def cert_san_get(debug, certificate):
-    """ get subject alternate names from certificate """
-    print_debug(debug, 'cert_san_get()')
-    pem_file = build_pem_file(debug, None, b64_url_recode(debug, certificate), True)
-    cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, pem_file)
-    san = []
-    ext_count = cert.get_extension_count()
-    for i in range(0, ext_count):
-        ext = cert.get_extension(i)
-        if 'subjectAltName' in str(ext.get_short_name()):
-            san.append(ext.__str__())
-
-    print_debug(debug, 'cert_san_get() ended')
-    return san
-
-def cert_serial_get(debug, certificate):
-    """ get serial number form certificate """
-    print_debug(debug, 'cert_serial_get()')
-    pem_file = build_pem_file(debug, None, b64_url_recode(debug, certificate), True)
-    cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, pem_file)
-    return cert.get_serial_number()
+def validate_csr(debug, order_dic, _csr):
+    """ validate certificate signing request against order"""
+    print_debug(debug, 'validate_csr({0})'.format(order_dic))
+    return True
 
 def validate_email(debug, contact_list):
     """ validate contact against RFC608"""
@@ -226,8 +231,3 @@ def validate_email(debug, contact_list):
         result = bool(re.search(pattern, contact_list))
         print_debug(debug, '# validate: {0} result: {1}'.format(contact_list, result))
     return result
-
-def validate_csr(debug, order_dic, _csr):
-    """ validate certificate signing request against order"""
-    print_debug(debug, 'validate_csr({0})'.format(order_dic))
-    return True
