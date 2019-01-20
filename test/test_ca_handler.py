6cc8e60d047cea2604b0ae1bd1fdf7a6dedf0785
grindsa <grindelsack@gmail.com>
2019-01-20 17:11:27 +0100

    fxed unittests

--- test/test_ca_handler.py
+++ test/test_ca_handler.py
@@ -14,39 +14,38 @@ except ImportError:
     from unittest.mock import patch, MagicMock, Mock
 sys.path.insert(0, '..')
 
-    
+
 class TestACMEHandler(unittest.TestCase):
     """ test class for cgi_handler """
-        
+
     def setUp(self):
         """ setup unittest """
         from acme.ca_handler import CAhandler
         self.cahandler = CAhandler(False)
         self.cahandler.api_host = 'api_host'
-        self.cahandler.auth = 'auth'         
-
+        self.cahandler.auth = 'auth'
 
     def test_001_default(self):
         """ default test which always passes """
         self.assertEqual('foo', 'foo')
 
-    @patch('acme.ca_handler.CAhandler.set_auth')       
+    @patch('acme.ca_handler.CAhandler.set_auth')
     @patch('requests.get')
     def test_002_get_ca(self, mock_get, mock_auth):
         """ CAhandler.get_ca() returns an http error """
         mock_get.side_effect = requests.exceptions.HTTPError
-        self.assertEqual({'status': 500, 'message': '', 'statusMessage': 'Internal Server Error'}, self.cahandler.get_ca('foo', 'bar'))        
+        self.assertEqual({'status': 500, 'message': '', 'statusMessage': 'Internal Server Error'}, self.cahandler.get_ca('foo', 'bar'))
 
     @patch('requests.get')
     def test_003_get_ca(self, mock_get):
         """ CAhandler.get_ca() returns an not json file """
-        mock_get.status_code = 200        
+        mock_get.status_code = 200
         mock_get.return_value.json = {"bbs": "hahha"}
-        self.assertEqual({'status': 500, 'message': "'str' object has no attribute 'json'", 'statusMessage': 'Internal Server Error'}, self.cahandler.get_ca('foo', 'bar'))        
+        self.assertEqual({'status': 500, 'message': "'dict' object is not callable", 'statusMessage': 'Internal Server Error'}, self.cahandler.get_ca('foo', 'bar'))
 
 
-        
-        
+
+
 if __name__ == '__main__':
 
     if os.path.exists('acme_test.db'):
