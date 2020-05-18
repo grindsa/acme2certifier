c326c422619f1a0988d0e5eab664e2c3686f5484
grindsa <grindelsack@gmail.com>
2020-05-18 17:22:38 +0200

    [feat] polling via external script

--- tools/cert_poll.py
+++ tools/cert_poll.py
@@ -8,14 +8,18 @@ from acme.certificate import Certificate
 if __name__ == '__main__':
 
     DEBUG = True
+
+    # timeout between the different polling request
+    TIMEOUT = 1
+
     # initialize logger
     LOGGER = logger_setup(DEBUG)
 
     with Certificate(DEBUG, 'foo', LOGGER) as certificate:
         # search certificates in status "processing"
-        CERT_LIST = certificate.certlist_search('order__status_id', 4, ('name', 'poll_identifier', 'csr'))
+        CERT_LIST = certificate.certlist_search('order__status_id', 4, ('name', 'poll_identifier', 'csr', 'order__name'))
 
         for cert in CERT_LIST:
             # check status of certificate
-            certificate.poll(cert['name'], cert['poll_identifier'], cert['csr'])
-
+            certificate.poll(cert['name'], cert['poll_identifier'], cert['csr'], cert['order__name'])
+            # time.sleep(TIMEOUT)
