10f156e6a3c6e05ba34281a657df4dfc067555e5
grindsa <grindelsack@gmail.com>
2020-05-26 20:23:06 +0200

    [fix] setup.py - packages and package_data

--- setup.py
+++ setup.py
@@ -1,22 +1,39 @@
+""" build script for acme2certifier """
 from setuptools import setup
 exec(open('acme/version.py').read())
 setup(name='acme2certifier',
-    version=__version__,
-    description='ACMEv2 server',
-    url='https://github.com/grindsa/acme2certifier',
-    author='grindsa',
-    author_email='grindelsack@gmail.com',
-    license='GPL',
-    packages=['acme2certifier'],
-    platforms='any',
-    classifiers = [
-        'Programming Language :: Python',
-        'Development Status :: 4 - Beta',
-        'Natural Language :: German',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-        ],    
-    zip_safe=False,
-    test_suite="test")
+      version=__version__,
+      description='ACMEv2 server',
+      url='https://github.com/grindsa/acme2certifier',
+      author='grindsa',
+      author_email='grindelsack@gmail.com',
+      license='GPL',
+      packages=['acme', 
+                'examples', 
+                'examples.ca_handler', 
+                'examples.db_handler', 
+                'examples.django.acme', 
+                'examples.django.acme.migrations', 
+                'examples.django.acme.fixture',
+                'examples.django.acme2certifier', 
+                'examples.django', 
+                'examples.nginx',
+                'tools',                 
+      ],
+      package_data={
+          'examples': ['*.py', '*.conf', '*.cfg'],
+          'examples.nginx': ['*.conf', '*.ini', '*.service'],
+          'examples.django.acme.fixture': ['*.yaml'],          
+      },
+      platforms='any',
+      classifiers=[
+          'Programming Language :: Python',
+          'Development Status :: 4 - Beta',
+          'Natural Language :: German',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Operating System :: OS Independent',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+          ],
+      zip_safe=False,
+      test_suite="test")
