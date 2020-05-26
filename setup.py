b1458a387fcffda37b12f259b7c6f2aa0b2e64fe
grindsa <grindelsack@gmail.com>
2020-05-26 17:39:38 +0200

    [fix] setup.py - update license

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 exec(open('acme/version.py').read())
 setup(name='acme2certifier',
     version=__version__,
-    description='acme v2 serer for NetGuard Certificate Manager / Insta Certifier.',
+    description='ACMEv2 server',
     url='https://github.com/grindsa/acme2certifier',
     author='grindsa',
     author_email='grindelsack@gmail.com',
-    license='MIT',
+    license='GPL',
     packages=['acme2certifier'],
     platforms='any',
     classifiers = [
@@ -14,7 +14,7 @@ setup(name='acme2certifier',
         'Development Status :: 4 - Beta',
         'Natural Language :: German',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules'
         ],    
