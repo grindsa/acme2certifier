dd19b48c052d02ff50d48cbbdaaa32cdd81de2db
grindsa <grindelsack@gmail.com>
2020-06-07 17:00:53 +0200

    [fix] pylint disable 0801 - check

--- acme/version.py
+++ acme/version.py
@@ -1,5 +1,4 @@
 """ version file """
-# pylint: disable=R0801 
 # Store the version here so:
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) we can import it in setup.py for the same reason
