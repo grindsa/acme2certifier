74c228c59d970633d27ec1b9c418290b62e3acab
grindsa <grindelsack@gmail.com>
2020-06-07 19:27:11 +0200

    [fix] wsgi.py

--- examples/django/acme2certifier/wsgi.py
+++ examples/django/acme2certifier/wsgi.py
@@ -4,13 +4,18 @@ WSGI config for acme2certifier project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-
+# pylint: disable=C0413
 import os
+import sys
+
+PROJECT_HOME = '/var/www/acme2certifier'
+
+if PROJECT_HOME not in sys.path:
+    sys.path.append(PROJECT_HOME)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acme2certifier.settings')
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "acme2certifier.settings")
-
 application = get_wsgi_application()
