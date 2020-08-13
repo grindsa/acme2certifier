64e2ea602d1ac9b389eae4f7cc7ff58371c2d870
grindsa <grindelsack@gmail.com>
2020-08-14 00:12:40 +0200

    [feat] migrate docker ngix to ubuntu

--- examples/Docker/nginx/wsgi/docker-entrypoint.sh
+++ examples/Docker/nginx/wsgi/docker-entrypoint.sh
@@ -1,48 +1,49 @@
 #!/bin/bash
 
 # create acme-srv.cfg if not existing
-if [ ! -f /opt/acme2certifier/volume/acme_srv.cfg ]
+if [ ! -f /var/www/acme2certifier/volume/acme_srv.cfg ]
 then
-    cp /opt/acme2certifier/examples/acme_srv.cfg /opt/acme2certifier/volume/
+    cp /var/www/acme2certifier/examples/acme_srv.cfg /var/www/acme2certifier/volume/
 fi
 
 # enable ssl if acme2certifier_cert.pem and acme2certifier_key.pem exist on volume
-if [ -f /opt/acme2certifier/volume/acme2certifier_cert.pem ] && [ -f /opt/acme2certifier/volume/acme2certifier_key.pem ]
+if [ -f /var/www/acme2certifier/volume/acme2certifier_cert.pem ] && [ -f /var/www/acme2certifier/volume/acme2certifier_key.pem ]
 then
-   if [ ! -f /etc/nginx/conf.d/acme_ssl.conf ]
+   if [ ! -f /etc/nginx/sites-available/acme_ssl.conf ]
    then
-     cp  /opt/acme2certifier/examples/nginx/nginx_acme_ssl.conf /etc/nginx/conf.d/acme_ssl.conf
+     cp  /var/www/acme2certifier/examples/nginx/nginx_acme_ssl.conf /etc/nginx/sites-available/acme_ssl.conf
+     ln -s /etc/nginx/sites-available/acme_ssl.conf /etc/nginx/sites-enabled/acme_ssl.conf
    fi
 fi
 
 # create ca_handler if not existing
-if [ ! -f /opt/acme2certifier/volume/ca_handler.py ]
+if [ ! -f /var/www/acme2certifier/volume/ca_handler.py ]
 then
-    cp /opt/acme2certifier/examples/ca_handler/skeleton_ca_handler.py /opt/acme2certifier/volume/ca_handler.py
+    cp /var/www/acme2certifier/examples/ca_handler/skeleton_ca_handler.py /var/www/acme2certifier/volume/ca_handler.py
 fi
 
 # create symlink for the acme_srv.cfg
-if [ ! -L /opt/acme2certifier/acme/acme_srv.cfg ]
+if [ ! -L /var/www/acme2certifier/acme/acme_srv.cfg ]
 then
-    ln -s /opt/acme2certifier/volume/acme_srv.cfg /opt/acme2certifier/acme/acme_srv.cfg
-    chown nginx.nginx /opt/acme2certifier/volume/acme_srv.cfg
+    ln -s /var/www/acme2certifier/volume/acme_srv.cfg /var/www/acme2certifier/acme/acme_srv.cfg
+    chown www-data.www-data /var/www/acme2certifier/volume/acme_srv.cfg
 fi
 
 # create symlink for the acme_srv.db
-if [ ! -L /opt/acme2certifier/acme/acme_srv.db ]
+if [ ! -L /var/www/acme2certifier/acme/acme_srv.db ]
 then
-    ln -s /opt/acme2certifier/volume/acme_srv.db /opt/acme2certifier/acme/acme_srv.db
+    ln -s /var/www/acme2certifier/volume/acme_srv.db /var/www/acme2certifier/acme/acme_srv.db
 fi
 
 # apply database updates (if needed)
-python3 /opt/acme2certifier/tools/db_update.py
+python3 /var/www/acme2certifier/tools/db_update.py
 
 # create symlink for the ca_handler
-if [ ! -L /opt/acme2certifier/acme/ca_handler.py ]
+if [ ! -L /var/www/acme2certifier/acme/ca_handler.py ]
 then
-    ln -s /opt/acme2certifier/volume/ca_handler.py /opt/acme2certifier/acme/ca_handler.py
+    ln -s /var/www/acme2certifier/volume/ca_handler.py /var/www/acme2certifier/acme/ca_handler.py
 fi
 
-chown -R nginx.nginx /opt/acme2certifier/volume
-chmod u+s /opt/acme2certifier/volume/
+chown -R www-data.www-data /var/www/acme2certifier/volume
+chmod u+s /var/www/acme2certifier/volume/
 exec "$@"
