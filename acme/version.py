2e5d72e80e73b7811c42dc50a0974aa060139ba3
grindsa <grindelsack@gmail.com>
2021-03-20 15:17:41 +0100

    [rel] bump to v0.14

--- acme/version.py
+++ acme/version.py
@@ -3,4 +3,4 @@
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
-__version__ = '0.14-devel'
+__version__ = '0.14'
