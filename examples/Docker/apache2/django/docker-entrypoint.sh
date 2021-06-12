62463508da940bc675324b643858bfdd93b208b9
grindsa <grindelsack@gmail.com>
2021-06-12 20:54:56 +0200

    [fix] docker-entrypoint.sh for django-apache2

--- examples/Docker/apache2/django/docker-entrypoint.sh
+++ examples/Docker/apache2/django/docker-entrypoint.sh
@@ -3,27 +3,21 @@
 # create acme-srv.cfg if not existing
 if [ ! -f /var/www/acme2certifier/volume/acme_srv.cfg ]
 then
-    echo "no acme_srv.cfg found! creating acme_srv.cfg"
+    echo "no acme_srv.cfg found! creating acme_srv.cfg" >> /proc/1/fd/1
     cp /var/www/acme2certifier/examples/acme_srv.cfg /var/www/acme2certifier/volume/
 fi
 
 # enable tls if acme2certifier.pem exists on volume
 if [ -f /var/www/acme2certifier/volume/acme2certifier.pem ]
 then
-    echo "found acme2certifier.pem! enalbe TLS"
+    echo "found acme2certifier.pem! enalbe TLS" >> /proc/1/fd/1
     cp  /var/www/acme2certifier/examples/apache_django_ssl.conf /etc/apache2/sites-enabled/acme2certifier_ssl.conf
 fi
 
-# create ca_handler if:
-# - ca_handler.py does not exists in volume AND
-# - no entry hanlder_file: exists in acme_srv.cfg
-# - define ca_handler defined under handler_file does not exists
-if ( [ ! -f /var/www/acme2certifier/volume/ca_handler.py ] && \
-     ! ( grep -E '^handler_file:' /var/www/acme2certifier/volume/acme_srv.cfg &> /dev/null && \
-         [ -f $(grep -E '^handler_file:' /var/www/acme2certifier/volume/acme_srv.cfg | awk -F":" '{print $2}') ] \
-        ))
+# create ca_handler if not existing
+if [ ! -f /var/www/acme2certifier/volume/ca_handler.py ]
 then
-    echo "no ca_handler.py found! creating from skeleton_ca_handler.py"
+    echo "no ca_handler.py found! creating from skeleton_ca_handler.py" >> /proc/1/fd/1
     cp /var/www/acme2certifier/examples/ca_handler/skeleton_ca_handler.py /var/www/acme2certifier/volume/ca_handler.py
 fi
 
@@ -49,7 +43,7 @@ fi
 # create settings.py if not existing
 if [ ! -f /var/www/acme2certifier/volume/settings.py ]
 then
-    echo "no settings.py found! copy settings.py"
+    echo "no settings.py found! copy settings.py"  >> /proc/1/fd/1
     egrep -v '(# SECURITY WARNING: keep the secret key used in production secret!|^SECRET_KEY)' /var/www/acme2certifier/examples/django/acme2certifier/settings.py > /var/www/acme2certifier/volume/settings.py
     ## generate SECRET_KEY
     echo "generating SECRET_KEY"
@@ -58,21 +52,45 @@ then
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '${DJANGO_SECRET_KEY}'
 EOF
-    echo "adding '*' wildcard hosts in settings.py"
+    echo "adding '*' wildcard hosts in settings.py"  >> /proc/1/fd/1
     sed -i "s/ALLOWED_HOSTS = \['127.0.0.1'\]/ALLOWED_HOSTS = \['127.0.0.1','*'\]/g" /var/www/acme2certifier/volume/settings.py
 fi
 
-# apply migration or create a symlink for settings.py
+# create migrations if not existing
+if [ ! -d /var/www/acme2certifier/volume/migrations ]
+then
+    echo "no acme_srv.cfg found! creating acme_srv.cfg" >> /proc/1/fd/1
+    cp  -R /var/www/acme2certifier/examples/django/acme/migrations /var/www/acme2certifier/volume/
+    # mkdir -p /var/www/acme2certifier/volume/migrations
+fi
+
+# create a symlink for migrations
+if [ ! -L /var/www/acme2certifier/acme/migrations ]
+then
+    if [ -d /var/www/acme2certifier/volume/migrations ]
+    then
+        echo "delete migration directory" >> /proc/1/fd/1    
+        rm -rf /var/www/acme2certifier/acme/migrations
+    fi 
+
+    echo "create symlink for migration directory" >> /proc/1/fd/1
+    ln -s /var/www/acme2certifier/volume/migrations /var/www/acme2certifier/acme/
+fi
+
+# apply migrations or create a symlink for settings.py
 if [ -L /var/www/acme2certifier/acme2certifier/settings.py ]
 then
     # apply migrations
+    echo "apply migrations"  >> /proc/1/fd/1
     python3 /var/www/acme2certifier/tools/django_update.py
     python3 manage.py loaddata acme/fixture/status.yaml
 else
     ln -s /var/www/acme2certifier/volume/settings.py /var/www/acme2certifier/acme2certifier/settings.py
+    python3 /var/www/acme2certifier/tools/django_update.py
+    python3 manage.py loaddata acme/fixture/status.yaml    
 fi
 
-chown -R www-data /var/www/acme2certifier/volume
+chown -R www-data.www-data /var/www/acme2certifier/volume
 chmod u+s /var/www/acme2certifier/volume/
 
 exec "$@"
