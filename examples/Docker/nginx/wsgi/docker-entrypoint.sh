afd825d50e635c0305f3c93b64bd0fb01e1ea725
grindsa <grindelsack@gmail.com>
2021-06-13 19:08:04 +0200

    [fix] dockerfiles and entrypoint scripts

--- examples/Docker/nginx/wsgi/docker-entrypoint.sh
+++ examples/Docker/nginx/wsgi/docker-entrypoint.sh
@@ -10,10 +10,10 @@ fi
 # enable ssl if acme2certifier_cert.pem and acme2certifier_key.pem exist on volume
 if  [ -f /var/www/acme2certifier/volume/acme2certifier_cert.pem ] && \
     [ -f /var/www/acme2certifier/volume/acme2certifier_key.pem ] && \
-    [ ! -f /etc/nginx/sites-available/acme_ssl.conf ]
+    [ ! -f /etc/nginx/sites-available/acme_srv_ssl.conf ]
 then
-    cp  /var/www/acme2certifier/examples/nginx/nginx_acme_ssl.conf /etc/nginx/sites-available/acme_ssl.conf
-    ln -s /etc/nginx/sites-available/acme_ssl.conf /etc/nginx/sites-enabled/acme_ssl.conf
+    cp  /var/www/acme2certifier/examples/nginx/nginx_acme_srv_ssl.conf /etc/nginx/sites-available/acme_srv_ssl.conf
+    ln -s /etc/nginx/sites-available/acme_srv_ssl.conf /etc/nginx/sites-enabled/acme_srv_ssl.conf
 fi
 
 # create ca_handler if:
@@ -30,25 +30,25 @@ then
 fi
 
 # create symlink for the acme_srv.cfg
-if [ ! -L /var/www/acme2certifier/acme/acme_srv.cfg ]
+if [ ! -L /var/www/acme2certifier/acme_srv/acme_srv.cfg ]
 then
-    ln -s /var/www/acme2certifier/volume/acme_srv.cfg /var/www/acme2certifier/acme/acme_srv.cfg
+    ln -s /var/www/acme2certifier/volume/acme_srv.cfg /var/www/acme2certifier/acme_srv/acme_srv.cfg
     chown www-data.www-data /var/www/acme2certifier/volume/acme_srv.cfg
 fi
 
 # create symlink for the acme_srv.db
-if [ ! -L /var/www/acme2certifier/acme/acme_srv.db ]
+if [ ! -L /var/www/acme2certifier/acme_srv/acme_srv.db ]
 then
-    ln -s /var/www/acme2certifier/volume/acme_srv.db /var/www/acme2certifier/acme/acme_srv.db
+    ln -s /var/www/acme2certifier/volume/acme_srv.db /var/www/acme2certifier/acme_srv/acme_srv.db
 fi
 
 # apply database updates (if needed)
 python3 /var/www/acme2certifier/tools/db_update.py
 
 # create symlink for the ca_handler
-if [ ! -L /var/www/acme2certifier/acme/ca_handler.py ]
+if [ ! -L /var/www/acme2certifier/acme_srv/ca_handler.py ]
 then
-    ln -s /var/www/acme2certifier/volume/ca_handler.py /var/www/acme2certifier/acme/ca_handler.py
+    ln -s /var/www/acme2certifier/volume/ca_handler.py /var/www/acme2certifier/acme_srv/ca_handler.py
 fi
 
 chown -R www-data /var/www/acme2certifier/volume
