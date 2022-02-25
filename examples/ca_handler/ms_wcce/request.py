56e827993955983ab142a2ac3ae46ca5e40fadf7
Markus Legner <markus.legner@siemens.com>
2022-02-25 15:27:23 +0100

    [feat] MS-WCCE CA handler based on Certipy code
    - copy and adapt code for MS-WCCE from https://github.com/ly4k/Certipy;
      code is located in examples/ca_handler/ms_wcce including the license
    - initial implementation of a MS-WCCE CA handler
      (examples/ca_handler/mswcce_ca_handler.py), only supporting `enroll`
      and without support for the CA chain

---
+++ examples/ca_handler/ms_wcce/request.py
@@ -0,0 +1,150 @@
+import logging
+
+from cryptography import x509
+from cryptography.hazmat.primitives.serialization import Encoding
+from impacket.dcerpc.v5 import rpcrt
+from impacket.dcerpc.v5.dtypes import DWORD, LPWSTR, PBYTE, ULONG
+from impacket.dcerpc.v5.ndr import NDRCALL, NDRSTRUCT
+from impacket.dcerpc.v5.nrpc import checkNullString
+from impacket.uuid import uuidtup_to_bin
+
+from examples.ca_handler.ms_wcce.errors import translate_error_code
+from examples.ca_handler.ms_wcce.rpc import get_dce_rpc
+from examples.ca_handler.ms_wcce.target import Target
+
+NAME = "req"
+MSRPC_UUID_ICPR = uuidtup_to_bin(("91ae6020-9e3c-11cf-8d7c-00aa00c091be", "0.0"))
+
+
+def csr_pem_to_der(csr: str) -> bytes:
+    csr = x509.load_pem_x509_csr(csr)
+    return csr.public_bytes(Encoding.DER)
+
+
+def der_to_pem(certificate: bytes) -> bytes:
+    cert = x509.load_der_x509_certificate(certificate)
+    return cert.public_bytes(Encoding.PEM)
+
+
+class DCERPCSessionError(rpcrt.DCERPCException):
+    def __init__(self, error_string=None, error_code=None, packet=None):
+        rpcrt.DCERPCException.__init__(self, error_string, error_code, packet)
+
+    def __str__(self) -> str:
+        self.error_code &= 0xFFFFFFFF
+        error_msg = translate_error_code(self.error_code)
+        return "RequestSessionError: %s" % error_msg
+
+
+# https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-wcce/d6bee093-d862-4122-8f2b-7b49102097dc
+class CERTTRANSBLOB(NDRSTRUCT):
+    structure = (
+        ("cb", ULONG),
+        ("pb", PBYTE),
+    )
+
+
+# https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-icpr/0c6f150e-3ead-4006-b37f-ebbf9e2cf2e7
+class CertServerRequest(NDRCALL):
+    opnum = 0
+    structure = (
+        ("dwFlags", DWORD),
+        ("pwszAuthority", LPWSTR),
+        ("pdwRequestId", DWORD),
+        ("pctbAttribs", CERTTRANSBLOB),
+        ("pctbRequest", CERTTRANSBLOB),
+    )
+
+
+# https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-icpr/0c6f150e-3ead-4006-b37f-ebbf9e2cf2e7
+class CertServerRequestResponse(NDRCALL):
+    structure = (
+        ("pdwRequestId", DWORD),
+        ("pdwDisposition", ULONG),
+        ("pctbCert", CERTTRANSBLOB),
+        ("pctbEncodedCert", CERTTRANSBLOB),
+        ("pctbDispositionMessage", CERTTRANSBLOB),
+    )
+
+
+class Request:
+    def __init__(
+        self,
+        target: Target = None,
+        ca: str = None,
+        template: str = None,
+        alt: str = None,
+        debug=False,
+        **kwargs
+    ):
+        self.target = target
+        self.ca = ca
+        self.template = template
+        self.alt_name = alt
+        self.request_id = 0
+        self.verbose = debug
+        self.kwargs = kwargs
+
+        self.dce = get_dce_rpc(
+            MSRPC_UUID_ICPR,
+            r"\pipe\cert",
+            self.target,
+            timeout=self.target.timeout,
+            verbose=self.verbose,
+        )
+
+    def get_cert(self, csr: bytes) -> bytes:
+        csr = csr_pem_to_der(csr)
+
+        attributes = ["CertificateTemplate:%s" % self.template]
+
+        if self.alt_name is not None:
+            attributes.append("SAN:upn=%s" % self.alt_name)
+
+        attributes = checkNullString("\n".join(attributes)).encode("utf-16le")
+        pctb_attribs = CERTTRANSBLOB()
+        pctb_attribs["cb"] = len(attributes)
+        pctb_attribs["pb"] = attributes
+
+        pctb_request = CERTTRANSBLOB()
+        pctb_request["cb"] = len(csr)
+        pctb_request["pb"] = csr
+
+        request = CertServerRequest()
+        request["dwFlags"] = 0
+        request["pwszAuthority"] = checkNullString(self.ca)
+        request["pdwRequestId"] = self.request_id
+        request["pctbAttribs"] = pctb_attribs
+        request["pctbRequest"] = pctb_request
+
+        logging.info("Requesting certificate")
+
+        response = self.dce.request(request)
+
+        error_code = response["pdwDisposition"]
+        request_id = response["pdwRequestId"]
+
+        if error_code == 3:
+            logging.info("Successfully requested certificate")
+        elif error_code == 5:
+            logging.warning("Certificate request is pending approval")
+        else:
+            error_msg = translate_error_code(error_code)
+            if "unknown error code" in error_msg:
+                logging.error(
+                    "Got unknown error while trying to request certificate: (%s): %s"
+                    % (
+                        error_msg,
+                        b"".join(response["pctbDispositionMessage"]["pb"]).decode(
+                            "utf-16le"
+                        ),
+                    )
+                )
+            else:
+                logging.error(
+                    "Got error while trying to request certificate: %s" % error_msg
+                )
+
+        logging.info("Request ID is %d" % request_id)
+
+        return der_to_pem(b"".join(response["pctbEncodedCert"]["pb"]))
---
+++ examples/ca_handler/ms_wcce/target.py
@@ -0,0 +1,157 @@
+import logging
+import socket
+
+from dns.resolver import Resolver
+
+
+def is_ip(hostname: str) -> bool:
+    try:
+        # Check if hostname is an IP
+        socket.inet_aton(hostname)
+        return True
+    except Exception:
+        pass
+    return False
+
+
+class DnsResolver:
+    def __init__(self):
+        self.resolver = Resolver()
+
+        self.mappings = {}
+
+    @staticmethod
+    def from_options(options, target) -> "DnsResolver":
+        self = DnsResolver()
+
+        # We can't put all possible nameservers in the list of nameservers, since
+        # the resolver will fail if one of them fails
+        nameserver = options.ns
+        if nameserver is None:
+            nameserver = target.dc_ip
+
+        if nameserver is not None:
+            self.resolver.nameservers = [nameserver]
+
+        self.use_tcp = options.dns_tcp
+
+        return self
+
+    @staticmethod
+    def create(
+        target: "Target" = None, ns: str = None, dns_tcp: bool = False
+    ) -> "DnsResolver":
+        self = DnsResolver()
+
+        # We can't put all possible nameservers in the list of nameservers, since
+        # the resolver will fail if one of them fails
+        nameserver = ns
+        if nameserver is None:
+            nameserver = target.dc_ip
+
+        if nameserver is not None:
+            self.resolver.nameservers = [nameserver]
+
+        self.use_tcp = dns_tcp
+
+        return self
+
+    def resolve(self, hostname: str) -> str:
+        # Try to resolve the hostname with DNS first, then try a local resolve
+        if hostname in self.mappings:
+            logging.debug(
+                "Resolved %s from cache: %s" % (repr(hostname), self.mappings[hostname])
+            )
+            return self.mappings[hostname]
+
+        if is_ip(hostname):
+            return hostname
+
+        ip_addr = None
+        if self.resolver.nameservers[0] is None:
+            logging.debug("Trying to resolve %s locally" % repr(hostname))
+        else:
+            logging.debug(
+                "Trying to resolve %s at %s"
+                % (repr(hostname), repr(self.resolver.nameservers[0]))
+            )
+        try:
+            answers = self.resolver.resolve(hostname, tcp=self.use_tcp)
+            if len(answers) == 0:
+                raise Exception()
+
+            ip_addr = answers[0].to_text()
+        except Exception:
+            pass
+
+        if ip_addr is None:
+            try:
+                ip_addr = socket.gethostbyname(hostname)
+            except Exception:
+                ip_addr = None
+
+        if ip_addr is None:
+            logging.warning("Failed to resolve: %s" % hostname)
+            return hostname
+
+        self.mappings[hostname] = ip_addr
+        return ip_addr
+
+
+class Target:
+    def __init__(
+        self,
+        domain: str = None,
+        username: str = None,
+        password: str = None,
+        hashes: str = None,
+        target_ip: str = None,
+        remote_name: str = None,
+        no_pass: bool = False,
+        dc_ip: str = None,
+        ns: str = None,
+        dns_tcp: bool = False,
+        timeout: int = 5,
+    ):
+        if domain is None:
+            domain = ""
+
+        if password == "" and username != "" and hashes is None and no_pass is not True:
+            from getpass import getpass
+
+            password = getpass("Password:")
+        hashes = hashes
+        if hashes is not None:
+            hashes = hashes.split(":")
+            if len(hashes) == 1:
+                (nthash,) = hashes
+                lmhash = nthash = nthash
+            else:
+                lmhash, nthash = hashes
+        else:
+            lmhash = nthash = ""
+
+        self.domain = domain
+        self.username = username
+        self.password = password
+        self.remote_name = remote_name
+        self.hashes = hashes
+        self.lmhash = lmhash
+        self.nthash = nthash
+        self.dc_ip = dc_ip
+        self.timeout = timeout
+
+        if ns is None:
+            ns = dc_ip
+
+        if is_ip(remote_name):
+            target_ip = remote_name
+
+        self.resolver = DnsResolver.create(self, ns=ns, dns_tcp=dns_tcp)
+
+        self.target_ip = target_ip
+        if self.target_ip is None and remote_name is not None:
+            self.target_ip = self.resolver.resolve(remote_name)
+
+    def __repr__(self) -> str:
+        return "<Target (%s)>" % repr(self.__dict__)
