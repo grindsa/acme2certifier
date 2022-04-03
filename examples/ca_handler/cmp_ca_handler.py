5a8d9e7b56eda08465eed13397d59cf90834bd95
grindsa <grindelsack@gmail.com>
2022-04-03 12:08:36 +0200

    [feat] #86 - cmp_ca_handler.py using inbuilt cmp feature from openssl 3.x

--- examples/ca_handler/cmp_ca_handler.py
+++ examples/ca_handler/cmp_ca_handler.py
@@ -3,10 +3,11 @@
 """ ca handler for  generic cmpv2 """
 from __future__ import print_function
 import os
-import time
+import shutil
 import subprocess
-# pylint: disable=E0401
-from acme_srv.helper import load_config, csr_dn_get, csr_pubkey_get, csr_san_get
+import tempfile
+# pylint: disable=E0401, C0209
+from acme_srv.helper import load_config, build_pem_file, b64_url_recode
 
 
 class CAhandler(object):
@@ -30,7 +31,7 @@ class CAhandler(object):
     def __exit__(self, *args):
         """ cose the connection at the end of the context """
 
-    def _certs_bundle(self, uts):
+    def _certs_bundle(self):
         """ create needed cert(bundle) """
         self.logger.debug('CAhandler._certs_bundle()')
 
@@ -39,16 +40,15 @@ class CAhandler(object):
         ca_pem = None
 
         # open ca files
-        if os.path.isfile('{0}/{1}_capubs.pem'.format(self.tmp_dir, uts)):
-            fso = open('{0}/{1}_capubs.pem'.format(self.tmp_dir, uts), 'r')
-            ca_pem = fso.read()
-            fso.close()
+        if os.path.isfile('{0}/capubs.pem'.format(self.tmp_dir)):
+            with open('{0}/capubs.pem'.format(self.tmp_dir), 'r', encoding='utf-8') as fso:
+                ca_pem = fso.read()
 
         # open certificate
-        if os.path.isfile('{0}/{1}_cert.pem'.format(self.tmp_dir, uts)):
-            fso = open('{0}/{1}_cert.pem'.format(self.tmp_dir, uts), 'r')
-            cert_raw = fso.read()
-            fso.close()
+        if os.path.isfile('{0}/cert.pem'.format(self.tmp_dir)):
+            with open('{0}/cert.pem'.format(self.tmp_dir,), 'r', encoding='utf-8') as fso:
+                cert_raw = fso.read()
+
             # create bundle and raw cert
             if cert_raw and ca_pem:
                 cert_bundle = cert_raw + ca_pem
@@ -61,27 +61,6 @@ class CAhandler(object):
         self.logger.debug('CAhandler._certs_bundle() ended with {0}/{1}'.format(bool(cert_bundle), bool(cert_raw)))
         return(cert_bundle, cert_raw)
 
-    def _csr_san_get(self, csr):
-        """ get subAltNames from CSR and format them as needed """
-        self.logger.debug('CAhandler._csr_san_get()')
-        san_list = csr_san_get(self.logger, csr)
-
-        o_list = []
-        for san in san_list:
-            try:
-                (_type, value) = san.lower().split(':')
-                if value:
-                    o_list.append(value)
-            except Exception:
-                self.logger.error('ERROR: CAhandler._csr_san_get(): SAN split failed: {0}'.format(san))
-
-        if o_list:
-            sans = '"{0}"'.format(', '.join(o_list))
-        else:
-            sans = None
-        self.logger.debug('CAhandler._csr_san_get() ended with: {0}'.format(sans))
-        return sans
-
     def _config_load(self):
         """" load config from file """
         # pylint: disable=R0912, R0915
@@ -93,8 +72,6 @@ class CAhandler(object):
                 if ele.startswith('cmp_'):
                     if ele == 'cmp_openssl_bin':
                         self.openssl_bin = config_dic['CAhandler']['cmp_openssl_bin']
-                    elif ele == 'cmp_tmp_dir':
-                        self.tmp_dir = config_dic['CAhandler']['cmp_tmp_dir']
                     elif ele == 'cmp_recipient':
                         if config_dic['CAhandler']['cmp_recipient'].startswith('/'):
                             value = config_dic['CAhandler'][ele]
@@ -135,67 +112,52 @@ class CAhandler(object):
         if 'popo' not in self.config_dic:
             self.config_dic['popo'] = 0
 
-        # create temp dir if needed
-        if self.tmp_dir:
-            if not os.path.exists(self.tmp_dir):
-                os.makedirs(self.tmp_dir)
-        else:
-            self.logger.error('CAhandler config error: "cmp_tmp_dir" parameter must be specified in config_file')
+        # create temporary directory
+        self.tmp_dir = tempfile.mkdtemp()
 
+        # defaulting openssl_bin
         if not self.openssl_bin:
             self.logger.warning('CAhandler config error: "cmp_openssl_bin" parameter not in config_file. Using default (/usr/bin/openssl)')
             self.openssl_bin = '/usr/bin/openssl'
 
-        if not self.recipient:
-            self.logger.error('CAhandler config error: "cmp_recipient" is missing in config_file.')
-
         self.logger.debug('CAhandler._config_load() ended')
 
-    def _opensslcmd_build(self, uts, subject, san_list):
+    def _opensslcmd_build(self):
         """ build openssl command """
         self.logger.debug('CAhandler._opensslcmd_build()')
 
         cmd_list = [self.openssl_bin, 'cmp']
-        for ele in self.config_dic:
+        for ele, value in self.config_dic.items():
             cmd_list.append('-{0}'.format(str(ele)))
-            if not self.config_dic[ele] is True:
-                cmd_list.append(str(self.config_dic[ele]))
+            if value is not True:
+                cmd_list.append(str(value))
 
-        # add additonal parameters to openssl_cmd
-        cmd_list.extend(['-subject', subject, '-newkey', '{0}/{1}_pubkey.pem'.format(self.tmp_dir, uts)])
-        if san_list:
-            cmd_list.extend(['-sans', san_list])
-        cmd_list.extend(['-extracertsout', '{0}/{1}_capubs.pem'.format(self.tmp_dir, uts), '-certout', '{0}/{1}_cert.pem'.format(self.tmp_dir, uts)])
+        cmd_list.extend(['-csr', '{0}/csr.pem'.format(self.tmp_dir), '-extracertsout', '{0}/capubs.pem'.format(self.tmp_dir), '-certout', '{0}/cert.pem'.format(self.tmp_dir)])
 
         # set timeouts if not configured
-        if '-msgtimeout' not in cmd_list:
-            cmd_list.extend(['-msgtimeout', '5'])
-        if '-totaltimeout' not in cmd_list:
-            cmd_list.extend(['-totaltimeout', '10'])
+        if '-msg_timeout' not in cmd_list:
+            cmd_list.extend(['-msg_timeout', '5'])
+        if '-total_timeout' not in cmd_list:
+            cmd_list.extend(['-total_timeout', '10'])
 
         self.logger.debug('CAhandler._opensslcmd_build() ended with: {0}'.format(' '.join(cmd_list)))
         return cmd_list
 
-    def _pubkey_save(self, uts, pubkey):
-        """ save public key to file """
-        self.logger.debug('CAhandler._pubkey_save()')
-        with open('{0}/{1}_pubkey.pem'.format(self.tmp_dir, str(uts)), 'w') as fso:
-            fso.write(pubkey)
-        self.logger.debug('CAhandler._pubkey_save() ended')
+    def _file_save(self, filename, content):
+        """ save content to file """
+        self.logger.debug('CAhandler._file_save({0})'.format(filename))
+        with open(filename, 'w', encoding='utf-8') as fso:
+            fso.write(content)
+        self.logger.debug('CAhandler._file_save() ended')
 
-    def _tmp_files_delete(self, uts):
+    def _tmp_dir_delete(self):
         """ delete temp files """
-        self.logger.debug('CAhandler._tmp_files_delete({0})'.format(uts))
+        self.logger.debug('CAhandler._tmp_dir_delete({0})'.format(self.tmp_dir))
 
-        if os.path.isfile('{0}/{1}_cert.pem'.format(self.tmp_dir, uts)):
-            os.remove('{0}/{1}_cert.pem'.format(self.tmp_dir, uts))
-            self.logger.debug('CAhandler._tmp_files_delete() cert')
-        if os.path.isfile('{0}/{1}_pubkey.pem'.format(self.tmp_dir, uts)):
-            os.remove('{0}/{1}_pubkey.pem'.format(self.tmp_dir, uts))
-            self.logger.debug('CAhandler._tmp_files_delete() pubkey')
-        if os.path.isfile('{0}/{1}_capubs.pem'.format(self.tmp_dir, uts)):
-            os.remove('{0}/{1}_capubs.pem'.format(self.tmp_dir, uts))
-            self.logger.debug('CAhandler._tmp_files_delete() capub')
+        if os.path.exists(self.tmp_dir):
+            shutil.rmtree(self.tmp_dir)
+        else:
+            self.logger.error('CAhandler._tmp_dir_delete(): failed: {0}'.format(self.tmp_dir))
 
     def enroll(self, csr):
         """ enroll certificate from via MS certsrv """
@@ -204,50 +166,28 @@ class CAhandler(object):
         error = None
         cert_raw = None
 
-        if self.openssl_bin:
-            # get unix time stamp
-            uts = str(int(time.time()))
-            # get subject
-            try:
-                subject = csr_dn_get(self.logger, csr)
-                if not subject:
-                    subject = '/CN=acme2certifier'
-            except Exception as err_:
-                self.logger.error('CAhandler.enroll(): csr_dn_get() failed with error: {0}'.format(err_))
-                subject = None
-            # get public key from csr
-            try:
-                pubkey = csr_pubkey_get(self.logger, csr)
-            except Exception as err_:
-                self.logger.error('CAhandler.enroll(): csr_pubkey_get() failed with error: {0}'.format(err_))
-                pubkey = None
-            # get subject alternate names
-            try:
-                san_list = self._csr_san_get(csr)
-            except Exception as err_:
-                self.logger.error('CAhandler.enroll(): _csr_san_get() failed with error: {0}'.format(err_))
-                san_list = []
+        if self.openssl_bin and csr:
 
-            if subject and pubkey and san_list:
-                # dump public key
-                self._pubkey_save(uts, pubkey)
-                # build openssl command and run it
-                openssl_cmd = self._opensslcmd_build(uts, subject, san_list)
-                rcode = subprocess.call(openssl_cmd)
-                if rcode:
-                    self.logger.error('CAhandler.enroll(): failed: {0}'.format(rcode))
-                    error = 'rc from enrollment not 0'
-                # generate certificates we need to return
-                if os.path.isfile('{0}/{1}_cert.pem'.format(self.tmp_dir, uts)):
-                    (cert_bundle, cert_raw) = self._certs_bundle(uts)
-                else:
-                    error = 'Enrollment failed'
-                # delete temporary files
-                self._tmp_files_delete(uts)
+            # prepare the CSR to be signed
+            csr = build_pem_file(self.logger, None, b64_url_recode(self.logger, csr), None, True)
+            # dump csr key
+            self._file_save('{0}/csr.pem'.format(self.tmp_dir), csr)
+
+            # build openssl command and run it
+            openssl_cmd = self._opensslcmd_build()
+            rcode = subprocess.call(openssl_cmd)
+            if rcode:
+                self.logger.error('CAhandler.enroll(): failed: {0}'.format(rcode))
+                error = 'rc from enrollment not 0'
+
+            # generate certificates we need to return
+            if os.path.isfile('{0}/cert.pem'.format(self.tmp_dir)):
+                (cert_bundle, cert_raw) = self._certs_bundle()
             else:
-                error = 'CSR invalid'
-        else:
-            error = 'Config incomplete'
+                error = 'Enrollment failed'
+
+            # delete temporary files
+            self._tmp_dir_delete()
 
         self.logger.debug('Certificate.enroll() ended with error: {0}'.format(error))
         return(error, cert_bundle, cert_raw, None)
@@ -267,6 +207,7 @@ class CAhandler(object):
     def revoke(self, _cert, _rev_reason, _rev_date):
         """ revoke certificate """
         self.logger.debug('CAhandler.tsg_id_lookup()')
+
         # get serial from pem file and convert to formated hex
 
         code = 500
