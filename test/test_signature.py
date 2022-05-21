b4343617c397748d3e1d522ab692f9024317822a
grindsa <grindelsack@gmail.com>
2022-05-21 09:08:16 +0200

    [tst] unittests

--- test/test_signature.py
+++ test/test_signature.py
@@ -80,7 +80,7 @@ class TestACMEHandler(unittest.TestCase):
         self.signature.dbstore.jwk_load.side_effect = Exception('exc_sig_jw_load')
         with self.assertLogs('test_a2c', level='INFO') as lcm:
             self.signature._jwk_load(1)
-        self.assertIn('CRITICAL:test_a2c:acme2certifier database error in Signature._hwk_load(): exc_sig_jw_load', lcm.output)
+        self.assertIn('CRITICAL:test_a2c:acme2certifier database error in Signature._jwk_load(): exc_sig_jw_load', lcm.output)
 
     @patch('acme_srv.signature.signature_check')
     def test_010_signature_eab_check(self, mock_sigchk):
@@ -147,6 +147,48 @@ class TestACMEHandler(unittest.TestCase):
         mock_load_cfg.return_value = parser
         self.signature.__init__()
         self.assertEqual('/acme/revokecert', self.signature.revocation_path )
+
+    def test_018_signature__cli_jwk_load(self):
+        """ test jwk load """
+        self.signature.dbstore.cli_jwk_load.return_value = 'foo'
+        self.assertEqual('foo', self.signature._cli_jwk_load(1))
+
+    def test_019_signature__cli_jwk_load(self):
+        """ test jwk load  - dbstore.jwk_load() raises an exception"""
+        self.signature.dbstore.cli_jwk_load.side_effect = Exception('exc_sig_jw_load')
+        with self.assertLogs('test_a2c', level='INFO') as lcm:
+            self.signature._cli_jwk_load(1)
+        self.assertIn('CRITICAL:test_a2c:acme2certifier database error in Signature._cli_jwk_load(): exc_sig_jw_load', lcm.output)
+
+    def test_020_signature_check(self):
+        """ test Signature.cli_check() without having aname """
+        self.assertEqual((False, 'urn:ietf:params:acme:error:accountDoesNotExist', None), self.signature.cli_check(None, 'content'))
+
+    def test_021_signature_check(self):
+        """ test Signature.check() without having content """
+        self.assertEqual((False, 'urn:ietf:params:acme:error:malformed', None), self.signature.cli_check('foo', None))
+
+    @patch('acme_srv.signature.Signature._cli_jwk_load')
+    def test_022_signature_check(self, mock_jwk):
+        """ test Signature.check() while pubkey lookup failed """
+        mock_jwk.return_value = {}
+        self.assertEqual((False, 'urn:ietf:params:acme:error:accountDoesNotExist', None), self.signature.cli_check('foo', 1))
+
+    @patch('acme_srv.signature.signature_check')
+    @patch('acme_srv.signature.Signature._cli_jwk_load')
+    def test_023_signature_check(self, mock_jwk, mock_sig):
+        """ test successful Signature.check()  """
+        mock_jwk.return_value = {'foo' : 'bar'}
+        mock_sig.return_value = (True, None)
+        self.assertEqual((True, None, None), self.signature.cli_check('foo', 1))
+
+    @patch('acme_srv.signature.signature_check')
+    @patch('acme_srv.signature.Signature._cli_jwk_load')
+    def test_024_signature_check(self, mock_jwk, mock_sig):
+        """ test successful Signature.check() without account_name  sigcheck returns something"""
+        mock_jwk.return_value = {'foo' : 'bar'}
+        mock_sig.return_value = ('result', 'error')
+        self.assertEqual(('result', 'error', None), self.signature.cli_check('foo', 1))
 
 
 if __name__ == '__main__':
