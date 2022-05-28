b34c78bbe500e08a0c2e9d3d04433470d49a52c0
grindsa <grindelsack@gmail.com>
2022-05-28 07:51:41 +0200

    [feat] cli_permissions in housekeeping.py and cli

--- acme_srv/housekeeping.py
+++ acme_srv/housekeeping.py
@@ -427,27 +427,32 @@ class Housekeeping(object):
 
         # def certreport_get(self, report_format='csv', report_name=None):
         # check message
-        (code, message, detail, _protected, payload, _account_name) = self.message.cli_check(content)
+        (code, message, detail, _protected, payload, _account_name, permissions_dic) = self.message.cli_check(content)
 
         response_dic = {}
         if code == 200:
             if 'type' in payload and 'data' in payload:
                 if payload['type'] == 'report':
-                    if 'name' in payload['data'] and payload['data']['name'] in ('certificates', 'accounts'):
-                        if 'format' in payload['data'] and payload['data']['format'] in ('csv', 'json'):
-                            if payload['data']['name'] == 'certificates':
-                                response_dic['data'] = self.certreport_get(report_format=payload['data']['format'])
-                            elif payload['data']['name'] == 'accounts':
-                                response_dic['data'] = self.accountreport_get(report_format=payload['data']['format'])
-                            code = 200
+                    if 'reportadmin' in permissions_dic and permissions_dic['reportadmin']:
+                        if 'name' in payload['data'] and payload['data']['name'] in ('certificates', 'accounts'):
+                            if 'format' in payload['data'] and payload['data']['format'] in ('csv', 'json'):
+                                if payload['data']['name'] == 'certificates':
+                                    response_dic['data'] = self.certreport_get(report_format=payload['data']['format'])
+                                elif payload['data']['name'] == 'accounts':
+                                    response_dic['data'] = self.accountreport_get(report_format=payload['data']['format'])
+                                code = 200
+                            else:
+                                code = 400
+                                message = 'urn:ietf:params:acme:error:malformed'
+                                detail = 'unknown report format'
                         else:
                             code = 400
                             message = 'urn:ietf:params:acme:error:malformed'
-                            detail = 'unknown report format'
+                            detail = 'unknown report type'
                     else:
-                        code = 400
-                        message = 'urn:ietf:params:acme:error:malformed'
-                        detail = 'unknown report type'
+                        code = 403
+                        message = 'urn:ietf:params:acme:error:unauthorized'
+                        detail = 'No permissions to download reports'
                 else:
                     code = 400
                     message = 'urn:ietf:params:acme:error:malformed'
@@ -460,6 +465,7 @@ class Housekeeping(object):
         # prepare/enrich response
         status_dic = {'code': code, 'type': message, 'detail': detail}
         response_dic = self.message.prepare_response(response_dic, status_dic, False)
+        print(response_dic)
         self.logger.debug('Housekeeping.parse() returned something.')
 
         return response_dic
