e86f4583278afa753990969ca24fc4621ff7f2b1
grindsa <grindelsack@gmail.com>
2022-10-27 06:45:07 +0200

    [fix] address code smells

--- examples/hooks/cn_dump_hooks.py
+++ examples/hooks/cn_dump_hooks.py
@@ -11,8 +11,7 @@ class Hooks:
     def __init__(self, logger) -> None:
         self.logger = logger
         self.save_path = None
-        if not self.save_path:
-            self._config_load()
+        self._config_load()
 
     def __enter__(self):
         """ Makes hook handler context manager """
@@ -36,19 +35,19 @@ class Hooks:
             fso.write(content)
         self.logger.debug('Hooks._file_append() ended')
 
-    def pre_hook(self, certificate_name, order_name, csr):
+    def pre_hook(self, _certificate_name, _order_name, csr):
         """ run before obtaining any certificates """
         self.logger.debug('Hook.pre_hook()')
         san_list = csr_san_get(self.logger, csr)
         self._file_append('{0}/pre_hook.txt'.format(self.save_path), json.dumps(san_list) + '\n')
 
-    def post_hook(self, certificate_name, order_name, csr, error):
+    def post_hook(self, _certificate_name, _order_name, csr, _error):
         """ run after *attempting* to obtain/renew certificates """
         self.logger.debug('Hook.post_hook()')
         san_list = csr_san_get(self.logger, csr)
         self._file_append('{0}/post_hook.txt'.format(self.save_path), json.dumps(san_list) + '\n')
 
-    def success_hook(self, certificate_name, order_name, csr, certificate, certificate_raw, poll_identifier):
+    def success_hook(self, _certificate_name, _order_name, _csr, _certificate, certificate_raw, _poll_identifier):
         """ run after each successfully certificate enrollment/renewal """
         self.logger.debug('Hook.success_hook()')
         san_list = cert_san_get(self.logger, certificate_raw)
