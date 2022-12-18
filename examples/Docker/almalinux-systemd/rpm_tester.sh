cf29157dba8858c36b1c0178a1ff4a762ce9986b
grindsa <grindelsack@gmail.com>
2022-12-18 16:24:31 +0100

    [wf] rpm included in first four handlers

--- examples/Docker/almalinux-systemd/rpm_tester.sh
+++ examples/Docker/almalinux-systemd/rpm_tester.sh
@@ -6,16 +6,20 @@ yum install -y procps rsyslog
 systemctl start rsyslog.service
 
 yum -y install epel-release
-yum -y localinstall /tmp/acme2certifier/acme2certifier-*.noarch.rpm
+yum -y localinstall /tmp/acme2certifier/*.rpm
 cp /opt/acme2certifier/examples/nginx/nginx_acme_srv.conf /etc/nginx/conf.d
+mkdir -p /opt/acme2certifier/volume/
 
 yes | cp /tmp/acme2certifier/acme_srv.cfg /opt/acme2certifier/acme_srv
-mkdir -p /opt/acme2certifier/volume/acme_ca/certs
-cp -R /tmp/acme2certifier/acme_ca/* /opt/acme2certifier/volume/acme_ca/
+if [ -d /tmp/acme2certifier/acme_ca ]
+  then
+  mkdir -p /opt/acme2certifier/volume/acme_ca/certs
+  cp -R /tmp/acme2certifier/acme_ca/* /opt/acme2certifier/volume/acme_ca/
+fi
+
 chown -R nginx.nginx /opt/acme2certifier/volume/
 ls -la /opt/acme2certifier/
 ls -la /opt/acme2certifier/volume
-ls -la /opt/acme2certifier/volume/acme_ca/
 
 systemctl enable acme2certifier.service
 systemctl start acme2certifier.service
