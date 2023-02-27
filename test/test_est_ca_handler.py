c5b5d7592ba4a477e452ccb99f29f0e7f7208b1c
grindsa <grindelsack@gmail.com>
2023-02-27 17:30:23 +0100

    [tst] unittests est_ca_handler.py

--- test/test_est_ca_handler.py
+++ test/test_est_ca_handler.py
@@ -486,9 +486,27 @@ class TestACMEHandler(unittest.TestCase):
             self.assertEqual(('exc_simple_enroll', None), self.cahandler._simpleenroll('csr'))
         self.assertIn('ERROR:test_a2c:CAhandler._simpleenroll() returned an error: exc_simple_enroll', lcm.output)
 
+    @patch('examples.ca_handler.est_ca_handler.CAhandler._cacerts_get')
+    def test_046_enroll(self, mock_ca):
+        """ test certificate enrollment _cacert_get returns error """
+        mock_ca.return_value = ('Error', None)
+        self.cahandler.est_host = 'foo'
+        with self.assertLogs('test_a2c', level='INFO') as lcm:
+            self.assertEqual(('Error', None, None, None), self.cahandler.enroll('csr'))
+        self.assertIn('ERROR:test_a2c:CAhandler.enroll() _cacerts_get error: Error', lcm.output)
+
+    @patch('examples.ca_handler.est_ca_handler.CAhandler._cacerts_get')
+    def test_047_enroll(self, mock_ca):
+        """ test certificate enrollment no error but now ca_certs """
+        mock_ca.return_value = (None, None)
+        self.cahandler.est_host = 'foo'
+        with self.assertLogs('test_a2c', level='INFO') as lcm:
+            self.assertEqual(('no CA certificates found', None, None, None), self.cahandler.enroll('csr'))
+        self.assertIn('ERROR:test_a2c:CAhandler.enroll(): no CA certificates found', lcm.output)
+
     @patch('examples.ca_handler.est_ca_handler.CAhandler._simpleenroll')
     @patch('examples.ca_handler.est_ca_handler.CAhandler._cacerts_get')
-    def test_046_enroll(self, mock_ca, mock_enroll):
+    def test_048_enroll(self, mock_ca, mock_enroll):
         """ test certificate enrollment _simpleenroll returns error """
         mock_ca.return_value = (None, 'ca_pem')
         mock_enroll.return_value = ('Error', None)
@@ -500,7 +518,7 @@ class TestACMEHandler(unittest.TestCase):
 
     @patch('examples.ca_handler.est_ca_handler.CAhandler._simpleenroll')
     @patch('examples.ca_handler.est_ca_handler.CAhandler._cacerts_get')
-    def test_047_enroll(self, mock_ca, mock_enroll):
+    def test_049_enroll(self, mock_ca, mock_enroll):
         """ test certificate enrollment _simpleenroll returns certificate """
         mock_ca.return_value = (None, 'ca_pem')
         mock_enroll.return_value = (None, 'cert')
@@ -510,7 +528,7 @@ class TestACMEHandler(unittest.TestCase):
 
     @patch('examples.ca_handler.est_ca_handler.CAhandler._simpleenroll')
     @patch('examples.ca_handler.est_ca_handler.CAhandler._cacerts_get')
-    def test_048_enroll(self, mock_ca, mock_enroll):
+    def test_050_enroll(self, mock_ca, mock_enroll):
         """ test certificate enrollment replace CERT BEGIN """
         mock_ca.return_value = (None, 'ca_pem')
         mock_enroll.return_value = (None, '-----BEGIN CERTIFICATE-----\ncert')
@@ -520,7 +538,7 @@ class TestACMEHandler(unittest.TestCase):
 
     @patch('examples.ca_handler.est_ca_handler.CAhandler._simpleenroll')
     @patch('examples.ca_handler.est_ca_handler.CAhandler._cacerts_get')
-    def test_049_enroll(self, mock_ca, mock_enroll):
+    def test_051_enroll(self, mock_ca, mock_enroll):
         """ test certificate enrollment replace CERT END """
         mock_ca.return_value = (None, 'ca_pem')
         mock_enroll.return_value = (None, 'cert-----END CERTIFICATE-----\n')
@@ -530,7 +548,7 @@ class TestACMEHandler(unittest.TestCase):
 
     @patch('examples.ca_handler.est_ca_handler.CAhandler._simpleenroll')
     @patch('examples.ca_handler.est_ca_handler.CAhandler._cacerts_get')
-    def test_050_enroll(self, mock_ca, mock_enroll):
+    def test_052_enroll(self, mock_ca, mock_enroll):
         """ test certificate enrollment replace CERT BEGIN AND END """
         mock_ca.return_value = (None, 'ca_pem')
         mock_enroll.return_value = (None, '-----BEGIN CERTIFICATE-----\ncert-----END CERTIFICATE-----\n')
@@ -540,7 +558,7 @@ class TestACMEHandler(unittest.TestCase):
 
     @patch('examples.ca_handler.est_ca_handler.CAhandler._simpleenroll')
     @patch('examples.ca_handler.est_ca_handler.CAhandler._cacerts_get')
-    def test_051_enroll(self, mock_ca, mock_enroll):
+    def test_053_enroll(self, mock_ca, mock_enroll):
         """ test certificate enrollment replace CERT BEGIN AND END and \n"""
         mock_ca.return_value = (None, 'ca_pem')
         mock_enroll.return_value = (None, '-----BEGIN CERTIFICATE-----\ncert\n-----END CERTIFICATE-----\n\n')
@@ -549,21 +567,21 @@ class TestACMEHandler(unittest.TestCase):
         self.assertEqual((None, '-----BEGIN CERTIFICATE-----\ncert\n-----END CERTIFICATE-----\n\nca_pem', 'cert', None), self.cahandler.enroll('csr'))
 
     @patch('examples.ca_handler.est_ca_handler.CAhandler._config_load')
-    def test_052__enter__(self, mock_cfg):
+    def test_054__enter__(self, mock_cfg):
         """ test enter  called """
         mock_cfg.return_value = True
         self.cahandler.__enter__()
         self.assertTrue(mock_cfg.called)
 
     @patch('examples.ca_handler.est_ca_handler.CAhandler._config_load')
-    def test_053__enter__(self, mock_cfg):
+    def test_055__enter__(self, mock_cfg):
         """ test enter api hosts defined """
         mock_cfg.return_value = True
         self.cahandler.est_host = 'api_host'
         self.cahandler.__enter__()
         self.assertFalse(mock_cfg.called)
 
-    def test_054_get_certificates(self):
+    def test_056_get_certificates(self):
         """ test pkcs7 convrt to pem """
         cert_pem_list = []
         with open(self.dir_path + '/ca/certs.p7b', 'r') as fso:
@@ -576,14 +594,14 @@ class TestACMEHandler(unittest.TestCase):
         self.assertEqual(result, cert_pem_list)
 
     @patch('OpenSSL.crypto._lib.sk_X509_num')
-    def test_055_get_certificates(self, mock_num):
+    def test_057_get_certificates(self, mock_num):
         """ test get_certificates to cover cornercases """
         mock_num.return_value = 0
         input = Mock()
         input.type_is_signed = Mock(return_value=None)
         self.assertFalse(self._get_certificates(input))
 
-    def test_056__pkcs7_to_pem(self):
+    def test_058__pkcs7_to_pem(self):
         """ test pkcs7 to pem default output """
         with open(self.dir_path + '/ca/certs.p7b', 'r') as fso:
             file_content = fso.read()
@@ -591,7 +609,7 @@ class TestACMEHandler(unittest.TestCase):
             result = fso.read()
         self.assertEqual(result, self.cahandler._pkcs7_to_pem(file_content))
 
-    def test_057__pkcs7_to_pem(self):
+    def test_059__pkcs7_to_pem(self):
         """ test pkcs7 to pem output string """
         with open(self.dir_path + '/ca/certs.p7b', 'r') as fso:
             file_content = fso.read()
@@ -599,14 +617,14 @@ class TestACMEHandler(unittest.TestCase):
             result = fso.read()
         self.assertEqual(result, self.cahandler._pkcs7_to_pem(file_content, 'string'))
 
-    def test_058__pkcs7_to_pem(self):
+    def test_060__pkcs7_to_pem(self):
         """ test pkcs7 to pem output list """
         with open(self.dir_path + '/ca/certs.p7b', 'r') as fso:
             file_content = fso.read()
         result = ['-----BEGIN CERTIFICATE-----\nMIIFTzCCAzegAwIBAgIIAzHyhSyrXfMwDQYJKoZIhvcNAQELBQAwKzEXMBUGA1UE\nCxMOYWNtZTJjZXJ0aWZpZXIxEDAOBgNVBAMTB3Jvb3QtY2EwHhcNMjAwNTI3MTM1\nNDAwWhcNMzAwNTI2MjM1OTAwWjAqMRcwFQYDVQQLEw5hY21lMmNlcnRpZmllcjEP\nMA0GA1UEAxMGc3ViLWNhMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA\nxXHaGZsolXe+PBdUryngHP9VbBC1mehqeTtYI+hqsqGNH7q9a7bSrxMwFuF1kYL8\njqqxkJdtl0L94xcxJg/ZdMx7Nt0vGI+BaAuTpEpUEHeN4tqS6NhB/m/0LGkAELc/\nqkzmoO4B1FDwEEj/3IXtZcupqG80oDt7jWSGXdtF7NTjzcumznMeRXidCdhxRxT/\n/WrsChaytXo0xWZ56oeNwd6x6Dr8/39PBOWtj4fldyDcg+Q+alci2tx9pxmu2bCV\nXcB9ftCLKhDk2WEHE88bgKSp7fV2RCmq9po+Tx8JJ7qecLunUsK/F0XN4kpoQLm9\nhcymqchnMSncSiyin1dQHGHWgXDtBDdq6A2Z6rx26Qk5H9HTYvcNSe1YwFEDoGLB\nZQjbCPWiaqoaH4agBQTclPvrrSCRaVmhUSO+pBtSXDkmN4t3MDZxfgRkp8ixwkB1\n5Y5f0LTpCyAJsdQDw8+Ea0aDqO30eskh4CErnm9+Fejd9Ew2cwpdwfBXzVSbYilM\nGueQihZHvJmVRxAwU69aO2Qs8B0tQ60CfWKVlmWPiakrvYYlPp0FBsM61G6LZEN8\nhH2CKnS8hHv5IWEXZvp0Pk8V3P5h6bWN0Tl+x/V1Prt7Wp8NoiPETE8XyDDxe6dm\nKxztWBH/mTsJyMGb6ZiUoXdPU9TFUKqHxTRLHaxfsPsCAwEAAaN4MHYwEgYDVR0T\nAQH/BAgwBgEB/wIBATAdBgNVHQ4EFgQUv96OjgYiIqutQ8jd1E+oq0hBPtUwDgYD\nVR0PAQH/BAQDAgGGMBEGCWCGSAGG+EIBAQQEAwIABzAeBglghkgBhvhCAQ0EERYP\neGNhIGNlcnRpZmljYXRlMA0GCSqGSIb3DQEBCwUAA4ICAQBbHLEVyg4f9uEujroc\n31UVyDRLMdPgEPLjOenSBCBmH0N81whDmxNI/7JAAB6J14WMX8OLF0HkZnb7G77W\nvDhy1aFvQFbXHBz3/zUO9Mw9J4L2XEW6ond3Nsh1m2oXeBde3R3ANxuIzHqZDlP9\n6YrRcHjnf4+1/5AKDJAvJD+gFb5YnYUKH2iSvHUvG17xcZx98Rf2eo8LealG4JqH\nJh4sKRy0VjDQD7jXSCbweTHEb8wz+6OfNGrIo+BhTFP5vPcwE4nlJwYBoaOJ5cVa\n7gdQJ7WkLSxvwHxuxzvSVK73u3jl3I9SqTrbMLG/jeJyV0P8EvdljOaGnCtQVRwC\nzM4ptXUvKhKOHy7/nyTF/Bc35ZwwL/2xWvNK1+NibgE/6CFxupwWpdmxQbVVuoQ3\n2tUil9ty0yC6m5GKE8+t1lrZuxyA+b/TBnYNO5xo8UEMbkpxaNYSwmw+f/loxXP/\nM7sIBcLvy2ugHEBxwd9o/kLXeXT2DaRvxPjp4yk8MpJRpNmz3aB5HJwaUnaRLVo5\nZ3XWWXmjMGZ6/m0AAoDbDz/pXtOoJZT8BJdD1DuDdszVsQnLVn4B/LtIXL6FbXsF\nzfv6ERP9a5gpKUZ+4NjgrnlGtdccNZpwyWF0IXcvaq3b8hXIRO4hMjzHeHfzJN4t\njX1vlY35Ofonc4+6dRVamBiF9A==\n-----END CERTIFICATE-----\n', '-----BEGIN CERTIFICATE-----\nMIIFcDCCA1igAwIBAgIIevLTTxOMoZgwDQYJKoZIhvcNAQELBQAwKzEXMBUGA1UE\nCxMOYWNtZTJjZXJ0aWZpZXIxEDAOBgNVBAMTB3Jvb3QtY2EwHhcNMjAwNTI3MDAw\nMDAwWhcNMzAwNTI2MjM1OTU5WjArMRcwFQYDVQQLEw5hY21lMmNlcnRpZmllcjEQ\nMA4GA1UEAxMHcm9vdC1jYTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIB\nAJy4UZHdZgYt64k/rFamoC676tYvtabeuiqVw1c6oVZI897cFLG6BYwyr2Eaj7tF\nrqTJDeMN4vZSudLsmLDq6m8KwX/riPzUTIlcjM5aIMANZr9rLEs3NWtcivolB5aQ\n1slhdVitUPLuxsFnYeQTyxFyP7lng9M/Z403KLG8phdmKjM0vJkaj4OuKOXf3UsW\nqWQYyRl/ms07xVj02uq08LkoeO+jtQisvyVXURdaCceZtyK/ZBQ7NFCsbK112cVR\n1e2aJol7NJAA6Wm6iBzAdkAA2l3kh40SLoEbaiaVMixLN2vilIZOOAoDXX4+T6ir\n+KnDVSJ2yu5c/OJMwuXwHrh7Lgg1vsFR5TNehknhjUuWOUO+0TkKPg2A7KTg72OZ\n2mOcLZIbxzr1P5RRvdmLQLPrTF2EJvpQPNmbXqN3ZVWEvfHTjkkTFY/dsOTvFTgS\nri15zYKch8votcU7z+BQhgmMtwO2JhPMmZ6ABd9skI7ijWpwOltAhxtdoBO6T6CB\nCrE2yXc6V/PyyAKcFglNmIght5oXsnE+ub/dtx8f9Iea/xNPdo5aGy8fdaitolDK\n16kd3Kb7OE4HMHIwOxxF1BEAqerxxhbLMRBr8hRSZI5cvLzWLvpAQ5zuhjD6V3b9\nBYFd4ujAu3zl3mbzdbYjFoGOX6aBZaGDxlc4O2W7HxntAgMBAAGjgZcwgZQwDwYD\nVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUDGVvuTFYZtEAkz3af9wRKDDvAswwHwYD\nVR0jBBgwFoAUDGVvuTFYZtEAkz3af9wRKDDvAswwDgYDVR0PAQH/BAQDAgGGMBEG\nCWCGSAGG+EIBAQQEAwIABzAeBglghkgBhvhCAQ0EERYPeGNhIGNlcnRpZmljYXRl\nMA0GCSqGSIb3DQEBCwUAA4ICAQAjko7dX+iCgT+m3Iy1Vg6j7MRevPAzq1lqHRRN\nNdt2ct530pIut7Fv5V2xYk35ka+i/G+XyOvTXa9vAUKiBtiRnUPsXu4UcS7CcrCX\nEzHx4eOtHnp5wDhO0Fx5/OUZTaP+L7Pd1GD/j953ibx5bMa/M9Rj+S486nst57tu\nDRmEAavFDiMd6L3jH4YSckjmIH2uSeDIaRa9k6ag077XmWhvVYQ9tuR7RGbSuuV3\nFc6pqcFbbWpoLhNRcFc+hbUKOsKl2cP+QEKP/H2s3WMllqgAKKZeO+1KOsGo1CDs\n475bIXyCBpFbH2HOPatmu3yZRQ9fj9ta9EW46n33DFRNLinFWa4WJs4yLVP1juge\n2TCOyA1t61iy++RRXSG3e7NFYrEZuCht1EdDAdzIUY89m9NCPwoDYS4CahgnfkkO\n7YQe6f6yqK6isyf8ZFcp1uF58eERDiF/FDqS8nLmCdURuI56DDoNvDpig5J/9RNW\nG8vEvt2p7QrjeZ3EAatx5JuYty/NKTHZwJWk51CgzEgzDwzE2JIiqeldtL5d0Sl6\neVuv0G04BEyuXxEWpgVVzBS4qEFIBSnTJzgu1PXmId3yLvg2Nr8NKvwyZmN5xKFp\n0A9BWo15zW1PXDaD+l39oTYD7agjXkzTAjYIcfNJ7ATIYFD0xAvNAOf70s7aNupF\nfvkG2Q==\n-----END CERTIFICATE-----\n']
         self.assertEqual(result, self.cahandler._pkcs7_to_pem(file_content, 'list'))
 
-    def test_059__pkcs7_to_pem(self):
+    def test_061__pkcs7_to_pem(self):
         """ test pkcs7 to pem output list """
         with open(self.dir_path + '/ca/certs.p7b', 'r') as fso:
             file_content = fso.read()
@@ -614,7 +632,7 @@ class TestACMEHandler(unittest.TestCase):
         self.assertEqual(result, self.cahandler._pkcs7_to_pem(file_content, 'unknown'))
 
     @patch('OpenSSL.crypto.load_pkcs7_data')
-    def test_060__pkcs7_to_pem(self, mock_load):
+    def test_062__pkcs7_to_pem(self, mock_load):
         """ test pkcs7 to pem output list """
         with open(self.dir_path + '/ca/certs.p7b', 'r') as fso:
             file_content = fso.read()
@@ -624,7 +642,7 @@ class TestACMEHandler(unittest.TestCase):
         self.assertEqual(result, self.cahandler._pkcs7_to_pem(file_content, 'unknown'))
 
     @patch('OpenSSL.crypto')
-    def test_061__pkcs7_to_pem(self, mock_load):
+    def test_063__pkcs7_to_pem(self, mock_load):
         """ test pkcs7 to pem output list """
         with open(self.dir_path + '/ca/certs.p7b', 'r') as fso:
             file_content = fso.read()
@@ -635,7 +653,7 @@ class TestACMEHandler(unittest.TestCase):
         self.assertEqual(result, self.cahandler._pkcs7_to_pem(file_content, 'unknown'))
 
     @patch('OpenSSL.crypto.load_pkcs7_data')
-    def test_062__pkcs7_to_pem(self, mock_load):
+    def test_064__pkcs7_to_pem(self, mock_load):
         """ test pkcs7 to pem exceptin """
         with open(self.dir_path + '/ca/certs.p7b', 'r') as fso:
             file_content = fso.read()
