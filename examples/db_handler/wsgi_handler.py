bbadb545cf38252bb20273f87a43fad87291bb06
grindsa <grindelsack@gmail.com>
2023-11-02 19:01:44 +0100

    [feat] db update - add header_info field

--- examples/db_handler/wsgi_handler.py
+++ examples/db_handler/wsgi_handler.py
@@ -284,7 +284,7 @@ class DBstore(object):
         ''')
         self.logger.debug('create certificate')
         self.cursor.execute('''
-            CREATE TABLE "certificate" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "name" varchar(15) NOT NULL UNIQUE, "cert" text, "cert_raw" text, "error" text, "order_id" integer NOT NULL REFERENCES "order" ("id"), "csr" text NOT NULL, "poll_identifier" text, "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, "renewal_info" text, "issue_uts" integer DEFAULT 0, "expire_uts" integer DEFAULT 0, "replaced" bolean DEFAULT 0)
+            CREATE TABLE "certificate" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "name" varchar(15) NOT NULL UNIQUE, "cert" text, "cert_raw" text, "error" text, "order_id" integer NOT NULL REFERENCES "order" ("id"), "csr" text NOT NULL, "poll_identifier" text,  "header_info" text, "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, "renewal_info" text, "issue_uts" integer DEFAULT 0, "expire_uts" integer DEFAULT 0, "replaced" bolean DEFAULT 0)
         ''')
         self.cursor.execute('''
             CREATE TABLE "housekeeping" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "name" varchar(15) NOT NULL UNIQUE, "value" text, "modified_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL)
@@ -380,6 +380,9 @@ class DBstore(object):
         if 'replaced' not in certificate_column_list:
             self.logger.info('alter certificate table - add replaced')
             self.cursor.execute('''ALTER TABLE certificate ADD COLUMN replaced boolean DEFAULT 0''')
+        if 'header_info' not in certificate_column_list:
+            self.logger.info('alter certificate table - add header_info')
+            self.cursor.execute('''ALTER TABLE certificate ADD COLUMN header_info text''')
 
     def _db_update_challenge(self):
         """ alter challenge table """
