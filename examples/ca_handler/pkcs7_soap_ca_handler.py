0074239ab3f32d0aa93ffebfacb9a840d5184390
grindsa <grindelsack@gmail.com>
2023-12-09 17:01:48 +0100

    [fix] soap_handler.py

--- examples/ca_handler/pkcs7_soap_ca_handler.py
+++ examples/ca_handler/pkcs7_soap_ca_handler.py
@@ -87,7 +87,7 @@ class CAhandler(object):
             self.logger.error('CAhandler._config_load(): signing_cert option is missing in config file')
 
         if 'password' in config_dic['CAhandler']:
-            self.password = config_dic['CAhandler']['password']
+            self.password = convert_string_to_byte(config_dic['CAhandler']['password'])
 
         if 'signing_key' in config_dic['CAhandler']:
             if os.path.exists(config_dic['CAhandler']['signing_key']):
