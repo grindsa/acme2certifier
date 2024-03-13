bd22005b4bdf464b87e766f7c51ae66b1f69e5c2
grindsa <grindelsack@gmail.com>
2024-03-13 14:13:45 +0100

    [fix] leading Zero in serialnumber

--- test/test_renewalinfo.py
+++ test/test_renewalinfo.py
@@ -200,21 +200,34 @@ class TestACMEHandler(unittest.TestCase):
         self.assertTrue(mock_update.called)
         self.assertTrue(mock_renstr_get.called)
 
+    @patch('acme_srv.renewalinfo.Renewalinfo._renewalinfo_get')
+    @patch('acme_srv.renewalinfo.Renewalinfo._renewalinfo_string_get')
+    @patch('acme_srv.renewalinfo.Renewalinfo._cert_table_update')
+    def test_016_get(self, mock_update, mock_renstr_get, mock_renget):
+        """ test get() """
+        mock_renget.side_effect = Exception('renewalinfo_get')
+        self.renewalinfo.dbstore.hkparameter_get.return_value = None
+        with self.assertLogs('test_a2c', level='INFO') as lcm:
+            self.assertEqual({'code': 400, 'data': 'urn:ietf:params:acme:error:malformed'}, self. renewalinfo.get('url'))
+        self.assertTrue(mock_update.called)
+        self.assertTrue(mock_renstr_get.called)
+        self.assertIn('ERROR:test_a2c:Renewalinfo.get() - error: renewalinfo_get', lcm.output)
+
     @patch('acme_srv.message.Message.check')
-    def test_016_update(self, mock_mcheck):
+    def test_017_update(self, mock_mcheck):
         """ test update() """
         mock_mcheck.return_value = (400, 'message', 'detail', 'protected', 'payload', 'account_name')
         self.assertEqual({'code': 400}, self.renewalinfo.update('content'))
 
     @patch('acme_srv.message.Message.check')
-    def test_017_update(self, mock_mcheck):
+    def test_018_update(self, mock_mcheck):
         """ test update() """
         mock_mcheck.return_value = (200, 'message', 'detail', 'protected', {'certid': 'certid'}, 'account_name')
         self.assertEqual({'code': 400}, self.renewalinfo.update('content'))
 
     @patch('acme_srv.renewalinfo.Renewalinfo._cert_dic_lookup')
     @patch('acme_srv.message.Message.check')
-    def test_018_update(self, mock_mcheck, mock_lookup):
+    def test_019_update(self, mock_mcheck, mock_lookup):
         """ test update() """
         mock_mcheck.return_value = (200, 'message', 'detail', 'protected', {'certid': 'certid', 'replaced': True}, 'account_name')
         mock_lookup.return_value = None
@@ -222,19 +235,10 @@ class TestACMEHandler(unittest.TestCase):
 
     @patch('acme_srv.renewalinfo.Renewalinfo._cert_dic_lookup')
     @patch('acme_srv.message.Message.check')
-    def test_019_update(self, mock_mcheck, mock_lookup):
+    def test_020_update(self, mock_mcheck, mock_lookup):
         """ test update() """
         mock_mcheck.return_value = (200, 'message', 'detail', 'protected', {'certid': 'certid', 'replaced': False}, 'account_name')
         mock_lookup.return_value = {'foo': 'bar'}
-        self.assertEqual({'code': 400}, self.renewalinfo.update('content'))
-
-    @patch('acme_srv.renewalinfo.Renewalinfo._cert_dic_lookup')
-    @patch('acme_srv.message.Message.check')
-    def test_020_update(self, mock_mcheck, mock_lookup):
-        """ test update() """
-        mock_mcheck.return_value = (200, 'message', 'detail', 'protected', {'certid': 'certid', 'replaced': True}, 'account_name')
-        mock_lookup.return_value = {'foo': 'bar'}
-        self.renewalinfo.dbstore.certificate_add.return_value = None
         self.assertEqual({'code': 400}, self.renewalinfo.update('content'))
 
     @patch('acme_srv.renewalinfo.Renewalinfo._cert_dic_lookup')
@@ -243,24 +247,33 @@ class TestACMEHandler(unittest.TestCase):
         """ test update() """
         mock_mcheck.return_value = (200, 'message', 'detail', 'protected', {'certid': 'certid', 'replaced': True}, 'account_name')
         mock_lookup.return_value = {'foo': 'bar'}
+        self.renewalinfo.dbstore.certificate_add.return_value = None
+        self.assertEqual({'code': 400}, self.renewalinfo.update('content'))
+
+    @patch('acme_srv.renewalinfo.Renewalinfo._cert_dic_lookup')
+    @patch('acme_srv.message.Message.check')
+    def test_022_update(self, mock_mcheck, mock_lookup):
+        """ test update() """
+        mock_mcheck.return_value = (200, 'message', 'detail', 'protected', {'certid': 'certid', 'replaced': True}, 'account_name')
+        mock_lookup.return_value = {'foo': 'bar'}
         self.renewalinfo.dbstore.certificate_add.return_value = 1
         self.assertEqual({'code': 200}, self.renewalinfo.update('content'))
 
-    def test_022_renewalinfo_string_get(self):
+    def test_023_renewalinfo_string_get(self):
         """ test update() """
         self.renewalinfo.server_name = 'http://server.name'
         self.renewalinfo.path_dic = {'renewalinfo': '/renewalinfo'}
         input_string = 'http://server.name/renewalinfo/foo'
         self.assertEqual('foo', self.renewalinfo._renewalinfo_string_get(input_string))
 
-    def test_023_renewalinfo_string_get(self):
+    def test_024_renewalinfo_string_get(self):
         """ test update() """
         self.renewalinfo.server_name = 'http://server.name'
         self.renewalinfo.path_dic = {'renewalinfo': '/renewalinfo'}
         input_string = 'http://server.name/renewalinfofoo'
         self.assertEqual('foo', self.renewalinfo._renewalinfo_string_get(input_string))
 
-    def test_024_renewalinfo_string_get(self):
+    def test_025_renewalinfo_string_get(self):
         """ test update() """
         self.renewalinfo.server_name = 'http://server.name'
         self.renewalinfo.path_dic = {'renewalinfo': '/renewalinfo/'}
@@ -271,7 +284,7 @@ class TestACMEHandler(unittest.TestCase):
     @patch('acme_srv.renewalinfo.Renewalinfo._draft01_lookup')
     @patch('acme_srv.renewalinfo.certid_hex_get')
     @patch('acme_srv.renewalinfo.Renewalinfo._serial_aki_get')
-    def test_025_cert_dic_lookup(self, mock_aki, mock_certid, mock_draft01, mock_draft02):
+    def test_026_cert_dic_lookup(self, mock_aki, mock_certid, mock_draft01, mock_draft02):
         """ test _cert_dic_lookup() """
         mock_aki.return_value = ('serail', 'aki')
         mock_draft01.return_value = {'foo': 'draft01'}
@@ -286,7 +299,7 @@ class TestACMEHandler(unittest.TestCase):
     @patch('acme_srv.renewalinfo.Renewalinfo._draft01_lookup')
     @patch('acme_srv.renewalinfo.certid_hex_get')
     @patch('acme_srv.renewalinfo.Renewalinfo._serial_aki_get')
-    def test_026_cert_dic_lookup(self, mock_aki, mock_certid, mock_draft01, mock_draft02):
+    def test_027_cert_dic_lookup(self, mock_aki, mock_certid, mock_draft01, mock_draft02):
         """ test _cert_dic_lookup() """
         mock_aki.return_value = ('serail', 'aki')
         mock_certid.return_value = ('mda', 'certid')
@@ -298,31 +311,14 @@ class TestACMEHandler(unittest.TestCase):
         self.assertTrue(mock_draft01.called)
         self.assertFalse(mock_draft02.called)
 
-    def test_027_draft01_lookup(self):
+    def test_028_draft01_lookup(self):
         """ test _draft01_lookup() """
         self.renewalinfo.dbstore.certificate_lookup.return_value = {'foo': 'bar'}
         self.assertEqual({'foo': 'bar'}, self.renewalinfo._draft01_lookup('certid_hex'))
 
-    def test_028_draft01_lookup(self):
-        """ test _draft01_lookup() """
-        self.renewalinfo.dbstore.certificate_lookup.side_effect = Exception('cert_lookup')
-        with self.assertLogs('test_a2c', level='INFO') as lcm:
-            self.assertFalse(self.renewalinfo._draft01_lookup('certid_hex'))
-        self.assertIn('CRITICAL:test_a2c:acme2certifier database error in Renewalinfo._draft01_lookup(): cert_lookup', lcm.output)
-
-    def test_029_draft02_lookup(self):
-        """ test _draft02_lookup() """
-        self.renewalinfo.dbstore.certificates_search.return_value = [{'foo': 'bar01', 'aki': 'aki01'}, {'foo': 'bar02', 'aki': 'aki02'}]
-        self.assertEqual({'aki': 'aki01', 'foo': 'bar01'}, self.renewalinfo._draft02_lookup('serial', 'aki01'))
-
-    def test_030_draft02_lookup(self):
-        """ test _draft02_lookup() """
-        self.renewalinfo.dbstore.certificates_search.return_value = [{'foo': 'bar01', 'aki': 'aki01'}, {'foo': 'bar02', 'aki': 'aki02'}]
-        self.assertEqual({'aki': 'aki02', 'foo': 'bar02'}, self.renewalinfo._draft02_lookup('serial', 'aki02'))
-
     @patch('acme_srv.renewalinfo.cert_aki_get')
     @patch('acme_srv.renewalinfo.cert_serial_get')
-    def test_031_cert_table_update(self, mock_serial, mock_aki):
+    def test_029_cert_table_update(self, mock_serial, mock_aki):
         """ test _cert_table_update() """
         self.renewalinfo.dbstore.certificates_search.return_value = [{'foo': 'bar', 'cert_raw': 'cert_raw', 'name': 'name', 'cert': 'cert'}]
         self.assertFalse(self.renewalinfo._cert_table_update())
@@ -331,7 +327,7 @@ class TestACMEHandler(unittest.TestCase):
 
     @patch('acme_srv.renewalinfo.cert_aki_get')
     @patch('acme_srv.renewalinfo.cert_serial_get')
-    def test_032_cert_table_update(self, mock_serial, mock_aki):
+    def test_030_cert_table_update(self, mock_serial, mock_aki):
         """ test _cert_table_update() """
         self.renewalinfo.dbstore.certificates_search.return_value = [{'foo': 'bar', 'cert_raw': None, 'name': 'name', 'cert': 'cert'}]
         self.assertFalse(self.renewalinfo._cert_table_update())
@@ -340,21 +336,48 @@ class TestACMEHandler(unittest.TestCase):
 
     @patch('acme_srv.renewalinfo.cert_aki_get')
     @patch('acme_srv.renewalinfo.cert_serial_get')
-    def test_033_cert_table_update(self, mock_serial, mock_aki):
+    def test_031_cert_table_update(self, mock_serial, mock_aki):
         """ test _cert_table_update() """
         self.renewalinfo.dbstore.certificates_search.return_value = [{'foo': 'bar', 'name': 'name', 'cert': 'cert'}]
         self.assertFalse(self.renewalinfo._cert_table_update())
         self.assertFalse(mock_serial.called)
         self.assertFalse(mock_aki.called)
 
-    def test_034_draft02_lookup(self):
+    def test_032_draft02_lookup(self):
         """ test _draft02_lookup() """
         self.renewalinfo.dbstore.certificates_search.return_value = [{'foo': 'bar01', 'aki': 'aki01'}, {'foo': 'bar02', 'aki': 'aki02'}]
         self.assertFalse(self.renewalinfo._draft02_lookup('serial', 'aki03'))
 
+    def test_033_draft01_lookup(self):
+        """ test _draft01_lookup() """
+        self.renewalinfo.dbstore.certificate_lookup.side_effect = Exception('cert_lookup')
+        with self.assertLogs('test_a2c', level='INFO') as lcm:
+            self.assertFalse(self.renewalinfo._draft01_lookup('certid_hex'))
+        self.assertIn('CRITICAL:test_a2c:acme2certifier database error in Renewalinfo._draft01_lookup(): cert_lookup', lcm.output)
+
+    def test_034_draft02_lookup(self):
+        """ test _draft02_lookup() """
+        self.renewalinfo.dbstore.certificates_search.return_value = [{'foo': 'bar01', 'aki': 'aki01'}, {'foo': 'bar02', 'aki': 'aki02'}]
+        self.assertEqual({'aki': 'aki01', 'foo': 'bar01'}, self.renewalinfo._draft02_lookup('serial', 'aki01'))
+
+    def test_035_draft02_lookup(self):
+        """ test _draft02_lookup() """
+        self.renewalinfo.dbstore.certificates_search.return_value = [{'foo': 'bar01', 'aki': 'aki01'}, {'foo': 'bar02', 'aki': 'aki02'}]
+        self.assertEqual({'aki': 'aki02', 'foo': 'bar02'}, self.renewalinfo._draft02_lookup('serial', 'aki02'))
+
+    def test_036_draft02_lookup(self):
+        """ test _draft02_lookup() failed with leading zero """
+        self.renewalinfo.dbstore.certificates_search.side_effect = [[], [{'foo': 'bar01', 'aki': 'aki01'}, {'foo': 'bar02', 'aki': 'aki02'}]]
+        self.assertEqual({'aki': 'aki02', 'foo': 'bar02'}, self.renewalinfo._draft02_lookup('00serial', 'aki02'))
+
+    def test_037_draft02_lookup(self):
+        """ test _draft02_lookup() without leading zero"""
+        self.renewalinfo.dbstore.certificates_search.side_effect = [[], [{'foo': 'bar01', 'aki': 'aki01'}, {'foo': 'bar02', 'aki': 'aki02'}]]
+        self.assertFalse(self.renewalinfo._draft02_lookup('serial', 'aki02'))
+
     @patch('acme_srv.renewalinfo.cert_aki_get')
     @patch('acme_srv.renewalinfo.cert_serial_get')
-    def test_035_cert_table_update(self, mock_serial, mock_aki):
+    def test_038_cert_table_update(self, mock_serial, mock_aki):
         """ test _cert_table_update() """
         self.renewalinfo.dbstore.certificates_search.side_effect = Exception('certificates_search')
         with self.assertLogs('test_a2c', level='INFO') as lcm:
@@ -363,14 +386,14 @@ class TestACMEHandler(unittest.TestCase):
         self.assertFalse(mock_serial.called)
         self.assertFalse(mock_aki.called)
 
-    def test_036_draft02_lookup(self):
+    def test_039_draft02_lookup(self):
         """ test _draft02_lookup() """
         self.renewalinfo.dbstore.certificates_search.return_value = [{'foo': 'bar01', }, {'foo': 'bar02', 'aki': 'aki02'}]
         with self.assertLogs('test_a2c', level='INFO') as lcm:
             self.assertFalse(self.renewalinfo._draft02_lookup('serial', 'aki03'))
         self.assertIn('CRITICAL:test_a2c:acme2certifier database error in Renewalinfo._draft02_lookup(): certificates_search', lcm.output)
 
-    def test_037_draft02_lookup(self):
+    def test_040_draft02_lookup(self):
         """ test _draft02_lookup() """
         self.renewalinfo.dbstore.certificates_search.side_effect = Exception('certificates_search')
         with self.assertLogs('test_a2c', level='INFO') as lcm:
@@ -379,7 +402,7 @@ class TestACMEHandler(unittest.TestCase):
 
     @patch('acme_srv.renewalinfo.b64_url_recode')
     @patch('acme_srv.renewalinfo.b64_decode')
-    def test_038_serial_aki_get(self, mock_dec, mock_rec):
+    def test_041_serial_aki_get(self, mock_dec, mock_rec):
         """ test _serial_aki_get() """
         mock_dec.side_effect = [b'aki', b'serial']
         mock_rec.side_effect = ['foo1', 'foo2']
@@ -389,7 +412,7 @@ class TestACMEHandler(unittest.TestCase):
 
     @patch('acme_srv.renewalinfo.b64_url_recode')
     @patch('acme_srv.renewalinfo.b64_decode')
-    def test_039_serial_aki_get(self, mock_dec, mock_rec):
+    def test_042_serial_aki_get(self, mock_dec, mock_rec):
         """ test _serial_aki_get() """
         mock_dec.side_effect = [b'aki', b'serial']
         mock_rec.side_effect = ['foo1', 'foo2']
@@ -399,7 +422,7 @@ class TestACMEHandler(unittest.TestCase):
 
     @patch('acme_srv.renewalinfo.b64_url_recode')
     @patch('acme_srv.renewalinfo.b64_decode')
-    def test_040_serial_aki_get(self, mock_dec, mock_rec):
+    def test_043_serial_aki_get(self, mock_dec, mock_rec):
         """ test _serial_aki_get() """
         mock_dec.side_effect = [b'aki', b'serial']
         mock_rec.side_effect = ['foo1', 'foo2']
@@ -407,27 +430,27 @@ class TestACMEHandler(unittest.TestCase):
         self.assertFalse(mock_dec.called)
         self.assertFalse(mock_rec.called)
 
-    def test_041_renewalinfo_generate(self):
+    def test_044_renewalinfo_generate(self):
         """ test _renewalinfo_generate() """
         self.assertFalse(self.renewalinfo._renewalinfo_generate({}))
 
-    def test_042_renewalinfo_generate(self):
+    def test_045_renewalinfo_generate(self):
         """ test _renewalinfo_generate() """
         cert_dic = {'foo': 'bar'}
         self.assertFalse(self.renewalinfo._renewalinfo_generate(cert_dic))
 
-    def test_043_renewalinfo_generate(self):
+    def test_046_renewalinfo_generate(self):
         """ test _renewalinfo_generate() """
         cert_dic  = {'expire_uts': 0}
         self.assertFalse(self.renewalinfo._renewalinfo_generate(cert_dic))
 
-    def test_044_renewalinfo_generate(self):
+    def test_047_renewalinfo_generate(self):
         """ test _renewalinfo_generate() """
         cert_dic = {'expire_uts': 1000, 'issue_uts': 100}
         self.assertEqual({'suggestedWindow': {'start': '1970-01-01T00:14:25Z', 'end': '1970-01-01T00:16:40Z'}}, self.renewalinfo._renewalinfo_generate(cert_dic))
 
     @patch('acme_srv.renewalinfo.uts_now')
-    def test_045_renewalinfo_generate(self, mock_uts):
+    def test_048_renewalinfo_generate(self, mock_uts):
         """ test _renewalinfo_generate() """
         mock_uts.return_value = 100
         cert_dic = {'expire_uts': 1000}
@@ -435,7 +458,7 @@ class TestACMEHandler(unittest.TestCase):
         self.assertTrue(mock_uts.called)
 
     @patch('acme_srv.renewalinfo.uts_now')
-    def test_046_renewalinfo_generate(self, mock_uts):
+    def test_049_renewalinfo_generate(self, mock_uts):
         """ test _renewalinfo_generate() """
         mock_uts.return_value = 86400000
         cert_dic = {'expire_uts': 1000, 'issue_uts': 200}
