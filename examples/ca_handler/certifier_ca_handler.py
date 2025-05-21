ade6a5c9911f8b1c919c835a52946b100eb41e18
grindsa <grindelsack@gmail.com>
2025-05-21 06:28:05 +0200

    [feat] profiles variable in handler

--- examples/ca_handler/certifier_ca_handler.py
+++ examples/ca_handler/certifier_ca_handler.py
@@ -54,6 +54,7 @@ class CAhandler(object):
         self.enrollment_config_log = False
         self.enrollment_config_log_skip_list = []
         self.allowed_domainlist = []
+        self.profiles = {}
 
     def __enter__(self):
         """Makes ACMEHandler a Context Manager"""
