9a8124fb0f084756b272b0be9aea1729f9b5e740
grindsa <grindelsack@gmail.com>
2025-10-03 18:32:09 +0200

    [fix] address sonarissues related to shell scripts

--- examples/trigger/certifier_trigger.sh
+++ examples/trigger/certifier_trigger.sh
@@ -8,7 +8,7 @@
 ACME2CERTIFIER_URL='http://192.168.14.1/trigger'
 
 # thats the relative path to cert
-CERT_FILE=$1
+CERT_FILE="${1}"
 
 # certifier base directory
 CERTIFIFER_BASE='/usr/local/certifier'
@@ -16,10 +16,10 @@ CERTIFIFER_BASE='/usr/local/certifier'
 CERT_PATH="${CERTIFIFER_BASE}/${CERT_FILE}"
 
 # certificate object in base64
-STR_BASE64=$(cat $CERT_PATH | base64 -w 0)
-PAYLOAD='{"payload": "'$STR_BASE64'", "signature": "foo"}'
+STR_BASE64=$(cat "${CERT_PATH}" | base64 -w 0)
+PAYLOAD='{"payload": "'"${STR_BASE64}"'", "signature": "foo"}'
 
 # post command
-curl -X POST -H "Content-Type: application/json" -d "$PAYLOAD" $ACME2CERTIFIER_URL
+curl -X POST -H "Content-Type: application/json" -d "${PAYLOAD}" "${ACME2CERTIFIER_URL}"
 
 exit 0
