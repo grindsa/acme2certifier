dcb71e6cbcf796f40db6c755bca1ce9044185bdf
grindsa <grindelsack@gmail.com>
2025-11-15 06:20:08 +0100

    [feat] schema update

--- examples/db_handler/wsgi_handler.py
+++ examples/db_handler/wsgi_handler.py
@@ -447,7 +447,7 @@ class DBstore(object):
         self.logger.debug("create challenge")
         self.cursor.execute(
             """
-            CREATE TABLE "challenge" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "name" varchar(15) NOT NULL UNIQUE, "token" varchar(64), "authorization_id" integer NOT NULL REFERENCES "authorization" ("id"), "expires" integer, "type" varchar(15) NOT NULL, "keyauthorization" varchar(128), "source" varchar(128), "status_id" integer NOT NULL REFERENCES "status" ("id"), "validated" integer DEFAULT 0, "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL)
+            CREATE TABLE "challenge" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "name" varchar(15) NOT NULL UNIQUE, "token" varchar(64), "authorization_id" integer NOT NULL REFERENCES "authorization" ("id"), "expires" integer, "type" varchar(15) NOT NULL, "keyauthorization" varchar(128), "source" varchar(128), "status_id" integer NOT NULL REFERENCES "status" ("id"), "validated" integer DEFAULT 0, "validation_error" text, "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL)
         """
         )
         self.logger.debug("create certificate")
@@ -608,6 +608,12 @@ class DBstore(object):
             self.logger.info("alter challenge table - add validated")
             self.cursor.execute(
                 """ALTER TABLE challenge ADD COLUMN validated integer DEFAULT 0"""
+            )
+
+        if "validation_error" not in challenges_column_list:
+            self.logger.info("alter challenge table - add validation_error")
+            self.cursor.execute(
+                """ALTER TABLE challenge ADD COLUMN validation_error text"""
             )
 
         if "source" not in challenges_column_list:
