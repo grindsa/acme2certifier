5ec59753041eefe04505822790a8d87aba04c08d
grindsa <grindelsack@gmail.com>
2025-11-23 08:13:19 +0100

    [feat] prevalidated_domainlist feature

--- acme_srv/helpers/config.py
+++ acme_srv/helpers/config.py
@@ -152,20 +152,22 @@ def config_allowed_domainlist_load(logger: logging.Logger, config_dic: Dict[str,
 
     allowed_domainlist = []
 
-    if "Authorization" in config_dic and "allowed_domainlist" in config_dic["Authorization"]:
+    if "Order" in config_dic and "allowed_domainlist" in config_dic["Order"]:
         try:
-            allowed_domainlist = json.loads(
-                config_dic["Authorization"]["allowed_domainlist"]
-            )
+            allowed_domainlist = json.loads(config_dic["Order"]["allowed_domainlist"])
         except Exception as err_:
             logger.warning(
                 "Failed to load allowed_domainlist from configuration: %s", err_
             )
             allowed_domainlist = PARSING_ERR_MSG
 
-    if not allowed_domainlist and "CAhandler" in config_dic and "allowed_domainlist" in config_dic["CAhandler"]:
+    if (
+        not allowed_domainlist
+        and "CAhandler" in config_dic
+        and "allowed_domainlist" in config_dic["CAhandler"]
+    ):
         logger.warning(
-            "allowed_domainlist parameter found in CAhandler section - this is deprecated, please use Authorization section"
+            "allowed_domainlist parameter found in CAhandler section - this is deprecated, please use Order section"
         )
         try:
             allowed_domainlist = json.loads(
