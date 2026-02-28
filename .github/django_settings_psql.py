fd88423e6a371c77a3bcb4334503d74706a18a99
grindsa <grindelsack@gmail.com>
2026-02-28 15:16:35 +0100

    [fix] remove passwords from django files

--- .github/django_settings_psql.py
+++ .github/django_settings_psql.py
@@ -77,7 +77,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "acme2certifier",
         "USER": "acme2certifier",
-        "PASSWORD": "1mmSvDF",
+        "XXX": "XXX", # --- IGNORE ---
         "HOST": "postgresdbsrv",
         "PORT": "",
     }
