0da3d018009617c0c0146b0677494e9eb30d1ed8
grindsa <grindelsack@gmail.com>
2026-03-08 10:38:15 +0100

    [fix] linting of openssl_ca_handler.py

--- examples/ca_handler/openssl_ca_handler.py
+++ examples/ca_handler/openssl_ca_handler.py
@@ -21,8 +21,6 @@ from cryptography.x509 import (
 )
 from cryptography.x509.oid import ExtendedKeyUsageOID, NameOID
 
-BLOCK_ALL_DOMAIN = "block.all"
-
 # pylint: disable=e0401
 from acme_srv.helper import (
     load_config,
