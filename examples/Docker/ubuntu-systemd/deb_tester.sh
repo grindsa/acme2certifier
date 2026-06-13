03d4f14a2d3fc2eca723ca385ef17f4529a398f2
grindsa <grindelsack@gmail.com>
2026-06-13 14:36:52 +0200

    [wf] fixes for certifier and digicert workflows

--- examples/Docker/ubuntu-systemd/deb_tester.sh
+++ examples/Docker/ubuntu-systemd/deb_tester.sh
@@ -33,7 +33,7 @@ case "${1}" in
     apt-get install -y /tmp/acme2certifier/acme2certifier*.deb
 
     apt-get install -y python3-pip
-    pip install requests-pkcs12 --no-deps --break-system-packages
+    pip install "requests-pkcs12" "cryptography<=48.0.1" --break-system-packages
     if [[ "${2}" = "apache2" ]]; then
       echo "configure apache"
       cp /var/www/acme2certifier/examples/apache2/apache_wsgi.conf /etc/apache2/sites-available/acme2certifier.conf
