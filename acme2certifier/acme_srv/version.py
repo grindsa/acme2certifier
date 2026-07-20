6c659b88a44412c38c82d3edb8dd8d39eb3364a6
grindsa <grindelsack@gmail.com>
2026-07-20 16:18:44 +0200

    [fix] set development version

--- acme2certifier/acme_srv/version.py
+++ acme2certifier/acme_srv/version.py
@@ -4,5 +4,5 @@
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) pyproject.toml can read it via tool.setuptools.dynamic
 # 3) runtime code can import the same value
-__version__ = "0.44"
+__version__ = "0.45.dev0"
 __dbversion__ = "0.41"
