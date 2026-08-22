4deccb008fab900d61fc4490f0a1c2159f1fcc53
grindsa <grindelsack@gmail.com>
2026-08-22 07:46:47 +0200

    [fix]  Make tkauth-01 validator fail closed and warn at startup when `tnauthlist_support` is enabled

--- acme2certifier/django_app/views.py
+++ acme2certifier/django_app/views.py
@@ -21,6 +21,7 @@ from acme2certifier.acme_srv.helper import (
     legacy_acme_get_load,
     acme_get_method_not_allowed_problem,
     server_name_configuration_validate,
+    tnauthlist_configuration_validate,
 )
 from acme2certifier.acme_srv.db_handler import log_active_db_handler
 from acme2certifier.acme_srv.housekeeping import (
@@ -84,6 +85,7 @@ ERR_RESPONSE_ACME_GET["Allow"] = "POST"
 # check configuration for parameters masked in ""
 config_check(LOGGER, CONFIG)
 server_name_configuration_validate(LOGGER, CONFIG)
+tnauthlist_configuration_validate(LOGGER, CONFIG)
 LEGACY_ACME_GET = legacy_acme_get_load(LOGGER, CONFIG)
 
 with Housekeeping(DEBUG, LOGGER) as housekeeping:
